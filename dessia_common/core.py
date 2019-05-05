da59528dc48020114748a7bd5f3a3c01a56a8d57
Steven Masfaraud <steven@masfaraud.fr>
2019-05-05 11:20:18 +0200

    Working on workflow

--- dessia_common/core.py
+++ dessia_common/core.py
@@ -1,68 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jan  5 12:17:30 2018
 
-@author: Steven Masfaraud masfaraud@dessia.tech
 """
 
-import ZODB
-import transaction
-#import ZODB.config
-from ZEO import client
-import random
 
-
-from BTrees.IOBTree import IOBTree
-
-class ResultsDBClient:
-    """
-    Abstract class of Results client for ZODB Database
-    """
-    def __init__(self,address,model_name):
-        self.address=address
-        self.storage = client(address)
-        self.db = ZODB.DB(self.storage)
-        self.model_name=model_name
-        #self.db = ZODB.config.databaseFromString(conf)
-        
-        connection=self.db.open()
-        try:
-            getattr(connection.root,self.model_name)
-        except AttributeError:
-            setattr(connection.root,self.model_name,IOBTree())
-            transaction.commit()
-        connection.close()
-        
-    def __del__(self):
-        self.CloseDB()
-        
-    def CloseDB(self):
-        self.db.close()
-        
-    def AddResult(self,result): 
-        connection=self.db.open()
-        try:
-            mk=getattr(connection.root,self.model_name).maxKey()
-            k=random.randint(0,mk+1)
-            if k in getattr(connection.root,self.model_name):
-                k=mk+1
-        except ValueError:
-            k=0
-
-        getattr(connection.root,self.model_name)[k]=result
-        transaction.commit()
-        connection.close()
-        return k
-    
-    def get_result(self,id_result):
-        connection=self.db.open()
-        result=getattr(connection.root,self.model_name)[id_result]
-        return result
-
-    def _get_results(self):
-        connection=self.db.open()
-        results=dict(getattr(connection.root,self.model_name))
-        connection.close()
-        return results
     
--- dessia_common/workflow.py
+++ dessia_common/workflow.py
@@ -4,9 +4,17 @@
 
 """
 
-class Block:
-    def __init__(self, object_class):
+class Model:
+    def __init__(self, object_class, object_id=None):
         self.object_class = object_class
+
+class Function:
+    def __init__(self, function):
+        self.function = function
+        self.input_args = []
+        for arg in inspect.getargspec(function).args:
+            
+        
 
 class Pipe:
     def __init__(self,
@@ -26,6 +34,37 @@ class MileStone:
         pass
 
 class WorkFlow:
-    def __init__(self, blocks, pipes):
-        self.blocks = blocks
+    def __init__(self, models, functions, pipes):
+        self.models = models
+        self.functions = functions
         self.pipes = pipes
+        
+
+class InputFunctionVariable:
+    def __init__(self, function, arg_name):
+        self.function = function
+        self.arg_name = arg_name
+
+        self.value = None
+
+class OutputFunctionVariable:
+    def __init__(self, function):
+        self.function = function
+        
+        self.value = None
+
+
+class ModelAttribute:
+    def __init__(self, model, name):
+        self.model = model
+        self.name = name
+    
+        
+import math
+
+def sinus_f(x):
+    return math.sin(x)
+
+sinus = Function(sinus_f)
+
+workflow = WorkFlow([], [sinus], [])
