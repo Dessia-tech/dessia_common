3a8b41b31e03fd5fedda7dc98c25ccbd2f8892f9
jezequel <jezequel@dessia.tech>
2020-02-06 09:50:19 +0100

    Display angular adapted to new front version

--- dessia_common/workflow.py
+++ dessia_common/workflow.py
@@ -1004,7 +1004,9 @@ class WorkflowRun(dc.DessiaObject):
                 values = [{f['attribute'] : dc.getdeepattr(v, f['attribute'])\
                            for f in filter_display[0]['filters']}\
                           for v in self.output_value]
-                filter_display[0]['values'] = values
+                filter_display[0]['datasets'] = [{'label' : 'Results',
+                                                  'values' : values,
+                                                  'color' : "#99b4d6"}]
                 filter_display[0]['references_attribute'] = 'output_value'
                 displays.extend(filter_display)
         return displays
