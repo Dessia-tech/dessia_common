45fe5ea773fe599632a23666b761820f591517ba
jezequel <jezequel@dessia.tech>
2020-03-06 12:24:48 +0100

    Fullclassname used on instance and not on classes

--- dessia_common/core.py
+++ dessia_common/core.py
@@ -90,7 +90,7 @@ class DessiaObject(protected_module.DessiaObject if not _open_source else object
     def __eq__(self, other_object):
         if not self._generic_eq:
             return object.__eq__(self, other_object)
-        if full_classname(self.__class__) != full_classname(other_object.__class__)\
+        if full_classname(self) != full_classname(other_object)\
         or self.__dict__.keys() != other_object.__dict__.keys(): # TODO : Check this line. Keys not ordered and/or just need to test used keys
             return False
 
@@ -288,7 +288,7 @@ class DessiaObject(protected_module.DessiaObject if not _open_source else object
             model = self.volmdlr_volume_model()
             display.append({'angular_component' : 'app-step-viewer',
                             'data' : model.babylon_data()})
-            
+
         if hasattr(self, 'plot_data'):
             display.append({'angular_component' : 'app-d3-plot-data',
                             'data' : self.plot_data()})
