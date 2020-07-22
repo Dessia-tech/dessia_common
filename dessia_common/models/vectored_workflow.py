7577d731edeaa9a02fd04a243fd26f14afa7e04c
jezequel <jezequel@dessia.tech>
2020-07-22 18:12:14 +0200

    Attempt to build catalog array handling with blocks

--- dessia_common/models/vectored_workflow.py
+++ dessia_common/models/vectored_workflow.py
@@ -22,22 +22,39 @@ objectives_attributes = wf.ModelAttribute(attribute_name='objectives',
                                           name='Objectives')
 array_attributes = wf.ModelAttribute(attribute_name='array',
                                      name='Array')
+
+indices_method = wf.ModelMethod(model_class=Catalog,
+                                method_name='get_variable_index',
+                                name='Indices')
+subblocks = [indices_method]
+subworkflow = wf.Workflow(blocks=subblocks, pipes=[],
+                          output=indices_method.outputs[0],
+                          name='ForEach workflow')
+workflow_block = wf.WorkflowBlock(workflow=subworkflow,
+                                  name='ForEach workflow_block')
+for_each_variable = wf.ForEach(workflow_block=workflow_block,
+                               workflow_iterable_input=workflow_block.inputs[0],
+                               name='ForEach variable')
+
 blocks = [import_csv, instantiate_pareto, instantiate_catalog,
-          objectives_method, objectives_attributes, array_attributes]
+          objectives_method, objectives_attributes, array_attributes,
+          for_each_variable]
 
 # Pipes
 pipe0 = wf.Pipe(import_csv.outputs[0], instantiate_catalog.inputs[0])
 pipe1 = wf.Pipe(import_csv.outputs[1], instantiate_catalog.inputs[1])
 pipe2 = wf.Pipe(instantiate_pareto.outputs[0], instantiate_catalog.inputs[2])
 pipe3 = wf.Pipe(instantiate_catalog.outputs[0], objectives_method.inputs[0])
-pipe4 = wf.Pipe(objectives_method.outputs[1], objectives_attributes.inputs[0])
-pipe5 = wf.Pipe(instantiate_catalog.outputs[0], array_attributes.inputs[0])
-pipes = [pipe0, pipe1, pipe2, pipe3, pipe4, pipe5]
+# pipe4 = wf.Pipe(instantiate_catalog.outputs[0], for_each_variable.inputs[0])
+pipe5 = wf.Pipe(objectives_method.outputs[1], objectives_attributes.inputs[0])
+pipe6 = wf.Pipe(instantiate_catalog.outputs[0], array_attributes.inputs[0])
+pipes = [pipe0, pipe1, pipe2, pipe3, pipe5, pipe6]
 
 # Workflow
 workflow = wf.Workflow(blocks=blocks, pipes=pipes,
                        output=array_attributes.outputs[0],
                        name='Cars workflow')
+workflow.plot_jointjs()
 
 # # Input values
 input_values = {workflow.index(import_csv.inputs[0]): 'cars.csv',
@@ -47,5 +64,6 @@ input_values = {workflow.index(import_csv.inputs[0]): 'cars.csv',
                 workflow.index(instantiate_catalog.inputs[3]): [],
                 workflow.index(instantiate_catalog.inputs[5]): 'Cars',
                 workflow.index(objectives_method.inputs[1]): aimpoint,
-                workflow.index(objectives_method.inputs[2]): minimized_attributes}
+                workflow.index(objectives_method.inputs[2]): minimized_attributes,
+                workflow.index(for_each_variable.inputs[0]): choice_args}
 workflow_run = workflow.run(input_variables_values=input_values)
