73196ce9c9ba8282d953cf976e126fe9a96bb249
Steven Masfaraud <masfaraud@dessia.tech>
2020-09-02 11:47:42 +0200

    Enforcing jinja2 version

--- setup.py
+++ setup.py
@@ -111,7 +111,8 @@ setup(name='dessia_common',
       author='Steven Masfaraud',
       author_email='masfaraud@dessia.tech',
       packages=['dessia_common'],
-      install_requires=['typeguard', 'networkx', 'numpy', 'pandas', 'jinja2',
+      install_requires=['typeguard', 'networkx', 'numpy', 'pandas',
+                        'jinja2=2.11.1',
                         'mypy_extensions', 'scipy', 'pyDOE'],
       python_requires='>=3.7')
 
