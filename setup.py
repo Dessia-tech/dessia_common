6546d4481b6624d5c05743212e17c63e2b77bb00
jezequel <jezequel@dessia.tech>
2020-12-21 10:58:50 +0100

    build(plot_data): add plot_data 0.4.2 to prerequisites

--- setup.py
+++ setup.py
@@ -110,6 +110,6 @@ setup(name='dessia_common',
       packages=['dessia_common'],
       install_requires=['typeguard', 'networkx', 'numpy', 'pandas',
                         'mypy_extensions', 'scipy', 'pyDOE',
-                        'plot_data>=0.4.1'],
+                        'plot_data>=0.4.2'],
       python_requires='>=3.7')
 
