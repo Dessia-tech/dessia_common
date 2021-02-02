fafd69e35cdd682a2a7eeba87e66b891ea35b976
Steven Masfaraud <masfaraud@dessia.tech>
2021-02-02 11:53:20 +0100

    First proposal of opimization/generation classes

---
+++ dessia_common/optimization.py
@@ -0,0 +1,53 @@
+
+
+import dessia_common.core as dc
+
+
+class Optimizer(dc.DessiaObject):
+    """
+    Common parts of optimizers
+    """
+    def adimensioned_vector(self, x):
+        pass
+
+    def reduced_vector(self, x):
+        pass
+
+    def cma_bounds(self):
+        pass
+
+    def scipy_minimize_bounds(self):
+        pass
+
+    def cma_optimization(self):
+        pass
+
+    def scipy_minimize_optimization(self):
+        pass
+
+class DrivenModelOptimizer(Optimizer):
+    """
+    Abstract class
+    """
+
+    def __init__(self, model):
+        self.model = model
+
+    def get_model_from_vector(self):
+        # modify inplace model from vector
+        raise NotImplementedError('the method must be overloaded by subclassing class')
+
+class InstantiatingModelOptimizer(Optimizer):
+    """
+    Abstract class, to be subclassed by real class
+    Instantiate a new model at each at each point request
+    """
+
+    def __init__(self):
+        # self.model = model
+        pass
+
+
+    def get_model_from_vector(self):
+        # instantiate a model from vector
+        raise NotImplementedError('the method must be overloaded by subclassing class')
