5b3ef82a8c0b36839179c930c4a24ac8831b7664
Ghislan Jézéquel <jezequel@dessia.tech>
2021-03-22 14:38:58 +0100

    feat(typings): add deprecation warning on Subclass

--- dessia_common/typings.py
+++ dessia_common/typings.py
@@ -1,13 +1,13 @@
-from typing import TypeVar, Generic, Dict, Any, SupportsFloat, Protocol, Tuple
+from typing import TypeVar, Generic, Dict, Any, Tuple
+import warnings
 
 
 T = TypeVar('T')
 
-# A = TypeVar('A', int, float, covariant=True)
-# D = TypeVar('D', int, float, contravariant=True)
-
 
 class Subclass(Generic[T]):
+    warnings.warn("Subclass is deprecated, use InstanceOf instead",
+                  DeprecationWarning)
     pass
 
 
@@ -19,29 +19,7 @@ RGBColor = Tuple[float, float, float]
 # Measures
 class Measure(float):
     units = ''
-#     def genere_doc(self) -> str:
-#         return self.__repr__()
-# def __new__(cls):
-#     return float.__new__(cls)
-
-# def __init__(self, value):
-#     float.__init__(self, value)
-
-# def __float__(self):
-#     return float.__float__(self)
-
-
-# class Distance(float):
-#     def __new__(cls, value):
-#         print('New')
-#         return super().__new__(cls, value)
-#
-#     def __init__(self, value):
-#         print('Init')
-#         float.__init__(self)
 
 
 class Distance(Measure):
     units = 'm'
-    # def __float__(self):
-    #     return float.__float__(self)
