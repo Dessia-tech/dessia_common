d62debd6edfa79eceb6657f53fedcd269c617584
Steven Masfaraud <masfaraud@dessia.tech>
2021-04-30 11:40:19 +0200

    feat: enhance generator class

--- dessia_common/generation.py
+++ dessia_common/generation.py
@@ -7,6 +7,9 @@ class Generator(dc.DessiaObject):
     """
     Common parts of generator
     """
+    def __init__(self, name:str=''):
+        dc.DessiaObject.__init__(self, name=name)
+        
     def model_valid(self, model):
         raise NotImplementedError('the method must be overloaded by subclassing class')
 
@@ -21,13 +24,13 @@ class RegularDecisionTreeGenerator(Generator):
     This is still experimental and might be buggy
     """
 
-    def __init__(self, number_possibilities):
+    def __init__(self, number_possibilities:int, name:str=''):
         self.number_possibilities = number_possibilities
         self.leaves_depth = len(self.number_possibilities) -1
         self.tree = dt.RegularDecisionTree(number_possibilities)
+        Generator.__init__(self, name=name)  
 
-
-    def generate(self):
+    def generate(self, **kwargs):
         while not self.tree.finished():
             model = self.model_from_vector(self.tree.current_node)
             valid = self.model_valid(model)
@@ -44,6 +47,6 @@ class DecisionTreeGenerator(Generator):
     This is still experimental and might be buggy
     """
 
-    def __init__(self):
-        # self.model = model
-        pass
+    def __init__(self, name:str=''):
+        self.tree = dt.DecisionTree()
+        Generator.__init__(self, name=name)
