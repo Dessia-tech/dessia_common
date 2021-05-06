62b54856d3836f4512a0bf6581a27232da81be11
Steven Masfaraud <masfaraud@dessia.tech>
2021-05-06 17:51:50 +0200

    feat: xlsx with object breakdown

--- dessia_common/core.py
+++ dessia_common/core.py
@@ -16,11 +16,8 @@ from copy import deepcopy
 import inspect
 import json
 import bson
-from openpyxl.writer.excel import save_virtual_workbook
-from openpyxl.styles.borders import Border, Side
-from openpyxl.styles import Alignment, PatternFill
-from openpyxl import Workbook
-import openpyxl.utils
+from dessia_common.exports import XLSXWriter
+
 
 from typing import List, Dict, Type, Tuple, Union, Any, \
     get_type_hints, get_origin, get_args
@@ -648,96 +645,10 @@ class DessiaObject:
         bson.BSON.encode(self.to_dict())
         json.dumps(self._displays())
 
+    
     def to_xlsx(self, filepath):
-        max_column_width = 40
-        color_dessIA1 = "95B3D8"
-        color_dessIA2 = "78909C"
-        grey1 = "CCCCCC"
-        thin_border = Border(left=Side(style='thin'),
-                             right=Side(style='thin'),
-                             top=Side(style='thin'),
-                             bottom=Side(style='thin'))
-
-        pattern_color1 = PatternFill(
-            fill_type="solid",
-            start_color=color_dessIA1,
-            end_color=color_dessIA1)
-
-        pattern_color2 = PatternFill(
-            fill_type="solid",
-            start_color=color_dessIA2,
-            end_color=color_dessIA2)
-
-
-
-        wb = Workbook()
-        # grab the active worksheet
-        ws1 = wb.active
-        ws1.title = 'Object {}'.format(self.__class__.__name__)
-
-        ws1['A1'] = 'Module'
-        ws1['B1'] = 'Class'
-        ws1['C1'] = 'name'
-
-        ws1['A1'].border = thin_border
-        ws1['B1'].border = thin_border
-        ws1['C1'].border = thin_border
-        ws1['A1'].fill = pattern_color1
-        ws1['B1'].fill = pattern_color1
-        ws1['C1'].fill = pattern_color1
-
-
-        ws1['A2'] = self.__module__
-        ws1['B2'] = self.__class__.__name__
-        ws1['C2'] = self.name
-
-        ws1['A2'].border = thin_border
-        ws1['B2'].border = thin_border
-        ws1['C2'].border = thin_border
-        ws1['A2'].fill = pattern_color1
-        ws1['B2'].fill = pattern_color1
-        ws1['C2'].fill = pattern_color1
-
-
-
-        ws1['A4'] = 'Attribute'
-        ws1['A5'] = 'Value'
-        ws1['A4'].border = thin_border
-        ws1['A5'].border = thin_border
-
-
-        # name_column_width = 0
-        i = 1
-        for (k, v) in sorted(self.__dict__.items()):
-            if (not k.startswith('_')) and k != 'name':
-                cell1 = ws1.cell(row=4, column=i, value=k)
-                cell2 = ws1.cell(row=5, column=i, value=str(v))
-
-                cell1.border = thin_border
-                cell1.fill = pattern_color2
-                cell2.border = thin_border
-
-                i += 1
-
-                column_width = min((len(k) + 1.5), max_column_width)
-                column_name = openpyxl.utils.cell.get_column_letter(i)
-                ws1.column_dimensions[column_name].width = column_width
-
-        if isinstance(filepath, str):
-            real_filepath = filepath
-            if not filepath.endswith('.xlsx'):
-                real_filepath += '.xlsx'
-        else:
-            real_filepath = tempfile.NamedTemporaryFile().name
-
-        wb.save(real_filepath)
-        
-        if not isinstance(filepath, str):
-            with open(real_filepath, 'rb') as file:
-                filepath.seek(0)
-                filepath.write(file.read())
-                
-                
+        writer = XLSXWriter(self)
+        writer.save_to_file(filepath)
 
 
     def to_step(self, filepath):
