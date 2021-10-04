62e5312d3af5661f5ec960f24a3a27778277a0b8
Steven Masfaraud <masfaraud@dessia.tech>
2021-10-04 17:07:56 +0200

    ci: coverage

---
+++ tests/ci_tests.py
@@ -0,0 +1,14 @@
+
+import os
+
+scripts = ['unit_tests.py', 'bson_valid.py'
+]
+
+#  'cyl_cyl.py', 'cyl_pf.py',
+# 'ls3D_ls3D.py', 'sweep_sweep.py', 'tore_cyl.py','tore_pf.py'
+# 'tore_tore.py'
+
+for script_name in scripts:
+    print('\n## Executing script {}'.format(script_name))
+
+    exec(open(script_name).read())
