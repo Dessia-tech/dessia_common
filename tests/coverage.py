dcc5b3e23e77118d78f15f176d0518338248a47d
Steven Masfaraud <masfaraud@dessia.tech>
2021-12-20 19:51:16 +0100

    ci: coverage enhancement

--- tests/coverage.py
+++ tests/coverage.py
@@ -25,13 +25,21 @@ print("untracked modules:", untracked_modules)
 with open("coverage.json", "r") as file:
     d = json.load(file)
 
-print("total covered", d["totals"]["percent_covered"], "%")
-assert d["totals"]["percent_covered"] >= MIN_MODULE_COVERAGE
+print('total covered', d['totals']['percent_covered'], '%')
+assert d['totals']['percent_covered'] > MIN_MODULE_COVERAGE
+print('[Coverage] You can increase MIN_MODULE_COVERAGE to maximum {}% (actual {}%)'.format(d['totals']['percent_covered'], MIN_MODULE_COVERAGE))
 
-for file_name, data in d["files"].items():
-    print(file_name, data["summary"]["percent_covered"], "%")
-    if "/".join(file_name.split("/")[-2:]) in untracked_modules:
-        print(file_name, "-> in untrack list")
+min_actual_coverage = 100
+for file_name, data in d['files'].items():
+    print(file_name, data['summary']['percent_covered'], '%')
+    # print('/'.join(file_name.split('/')[-2:])) 
+    if '/'.join(file_name.split('/')[-2:]) in untracked_modules:
+        print(file_name, '-> in untrack list')
     else:
         # print('Testing if {} is above {}'.format(file_name, MIN_FILE_COVERAGE))
-        assert (data["summary"]["percent_covered"]) >= MIN_FILE_COVERAGE
+        assert(data['summary']['percent_covered']) > MIN_FILE_COVERAGE
+        min_actual_coverage = min(min_actual_coverage, data['summary']['percent_covered'])
+
+print('[Coverage] You can increase MIN_FILE_COVERAGE to maximum {}% (actual:{})%'.format(min_actual_coverage, MIN_FILE_COVERAGE))
+
+
