1033a4ede8229f4a70d1f6161595f6d2ca04770e
Steven Masfaraud <masfaraud@dessia.tech>
2022-01-04 18:59:48 +0100

    ci: explicit coverage error

--- tests/coverage.py
+++ tests/coverage.py
@@ -26,20 +26,26 @@ with open("coverage.json", "r") as file:
     d = json.load(file)
 
 print('total covered', d['totals']['percent_covered'], '%')
-assert d['totals']['percent_covered'] > MIN_MODULE_COVERAGE
-print('[Coverage] You can increase MIN_MODULE_COVERAGE to maximum {}% (actual {}%)'.format(d['totals']['percent_covered'], MIN_MODULE_COVERAGE))
+if d['totals']['percent_covered'] < MIN_MODULE_COVERAGE:
+    raise RuntimeError('Package is not covered enough by tests: {}% expected minimum {}%'.format(
+        d['totals']['percent_covered'], MIN_MODULE_COVERAGE))
+
+print('[Coverage] You can increase MIN_MODULE_COVERAGE to maximum {}% (actual {}%)'.format(
+    d['totals']['percent_covered'], MIN_MODULE_COVERAGE))
 
 min_actual_coverage = 100
 for file_name, data in d['files'].items():
     print(file_name, data['summary']['percent_covered'], '%')
-    # print('/'.join(file_name.split('/')[-2:])) 
+    # print('/'.join(file_name.split('/')[-2:]))
     if '/'.join(file_name.split('/')[-2:]) in untracked_modules:
         print(file_name, '-> in untrack list')
     else:
         # print('Testing if {} is above {}'.format(file_name, MIN_FILE_COVERAGE))
-        assert(data['summary']['percent_covered']) > MIN_FILE_COVERAGE
-        min_actual_coverage = min(min_actual_coverage, data['summary']['percent_covered'])
+        if data['summary']['percent_covered'] < MIN_FILE_COVERAGE:
+            raise RuntimeError('Module {} is not covered enough by tests: {}% expected minimum {}%'.format(
+                file_name, d['totals']['percent_covered'], MIN_MODULE_COVERAGE))
+        min_actual_coverage = min(
+            min_actual_coverage, data['summary']['percent_covered'])
 
-print('[Coverage] You can increase MIN_FILE_COVERAGE to maximum {}% (actual:{})%'.format(min_actual_coverage, MIN_FILE_COVERAGE))
-
-
+print('[Coverage] You can increase MIN_FILE_COVERAGE to maximum {}% (actual:{})%'.format(
+    min_actual_coverage, MIN_FILE_COVERAGE))
