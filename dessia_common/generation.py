b037409b8c500d314cc590f3665e9cbafc27fa8c
Steven Masfaraud <masfaraud@dessia.tech>
2022-01-10 14:21:17 +0100

    fix: inheritence in generation

--- dessia_common/generation.py
+++ dessia_common/generation.py
@@ -11,19 +11,23 @@ class Generator(dc.DessiaObject):
     def __init__(self, name:str=''):
         dc.DessiaObject.__init__(self, name=name)
 
+    def is_model_valid(self, model)->bool:        
+        raise NotImplementedError('the method is_model_valid must be overloaded by subclassing class')
 
-class DecisionTreeGenerator(Generator):
-    """
-    Abstract class, to be subclassed by real class
-    This is still experimental and might be buggy
-    """
+    def number_possibilities_from_model(self, model):
+        raise NotImplementedError('the method number_possibilities_from_model must be overloaded by subclassing class')
 
+
+class TreeGenerator(Generator):
+    """
+    Common parts of generator
+    """
     _allowed_methods = ['generate']
     _non_serializable_attributes = ['tree']
     _non_data_eq_attributes = ['tree']
 
-    def __init__(self, name:str=''):
-        self.tree = dt.DecisionTree()
+    def __init__(self, tree, name:str=''):
+        self.tree = tree
         Generator.__init__(self, name=name)
 
     def model_from_vector(self, vector:List[int]):        
@@ -32,20 +36,25 @@ class DecisionTreeGenerator(Generator):
     def is_vector_valid(self, vector:List[int]) -> bool:
         return True
 
-    def is_model_valid(self, model)->bool:        
-        raise NotImplementedError('the method is_model_valid must be overloaded by subclassing class')
-
-
-    def number_possibilities_from_model(self, model):
-        raise NotImplementedError('the method number_possibilities_from_model must be overloaded by subclassing class')
-
     def number_possibilities_from_vector(self, vector):
         """
         This method is generic but can be overloaded to avoid model instanciation
         """
         model = self.model_from_vector()
         return self.number_possibilities_from_model(model)
-        
+
+
+class DecisionTreeGenerator(TreeGenerator):
+    """
+    Abstract class, to be subclassed by real class
+    This is still experimental and might be buggy
+    """
+
+    def __init__(self, name:str=''):
+        tree = dt.DecisionTree()
+        TreeGenerator.__init__(self, tree, name=name)
+
+
         
     def generate(self, verbose:bool=False):
         model = self.model_from_vector(self.tree.current_node)
@@ -74,7 +83,7 @@ class DecisionTreeGenerator(Generator):
             self.tree.NextNode(valid)
                 
                 
-class RegularDecisionTreeGenerator(DecisionTreeGenerator):
+class RegularDecisionTreeGenerator(TreeGenerator):
     """
     Abstract class, to be subclassed by real class
     This is still experimental and might be buggy
@@ -83,8 +92,8 @@ class RegularDecisionTreeGenerator(DecisionTreeGenerator):
     def __init__(self, number_possibilities:List[int], name:str=''):
         self.number_possibilities = number_possibilities
         self.leaves_depth = len(self.number_possibilities) - 1
-        self.tree = dt.RegularDecisionTree(number_possibilities)
-        DecisionTreeGenerator.__init__(self, name=name)
+        tree = dt.RegularDecisionTree(number_possibilities)
+        TreeGenerator.__init__(self, tree=tree, name=name)
 
     def generate(self, sorted_nodes:bool=False,
                  unique_nodes:bool=False,
