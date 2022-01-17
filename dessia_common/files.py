d6fa557b21bf6237acc077c2a4076aacaef45350
Adnane LOUARTI <adnane.louarti@dessia.tech>
2022-01-17 10:59:45 +0100

    fix: add doc string for files

--- dessia_common/files.py
+++ dessia_common/files.py
@@ -8,6 +8,9 @@ import io
 
 
 class BinaryFile(io.BytesIO):
+    """
+    class for handling binary files with name
+    """
     def __init__(self, filename: str = ''):
         super().__init__()
         self.filename = filename
@@ -20,6 +23,10 @@ class BinaryFile(io.BytesIO):
 
 
 class StringFile(io.StringIO):
+    """
+    class for handling text files with name
+    default encoding : utf-8
+    """
     def __init__(self, filename: str = ''):
         super().__init__()
         self.filename = filename
@@ -29,6 +36,7 @@ class StringFile(io.StringIO):
         file_copy.write(self.data)
         file_copy.seek(0)
         return file_copy
+
 
 class XLSXFile(BinaryFile):
     extension = 'xlsx'
