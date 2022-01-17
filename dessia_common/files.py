c7e0a279a6d309523b9d8d318c70e5389e0ac9a6
Adnane LOUARTI <adnane.louarti@dessia.tech>
2022-01-17 11:25:28 +0100

    fix: more doc string fixes

--- dessia_common/files.py
+++ dessia_common/files.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
 
-"""
+"""Module defining recognized  file types in DC"""
 
 import io
 
 
 class BinaryFile(io.BytesIO):
-    """
-    class for handling binary files with name
-    """
+    """Class for handling binary files with name"""
+
     def __init__(self, filename: str = ''):
         super().__init__()
         self.filename = filename
 
     def copy(self):
+        """ Files deep copy  """
         file_copy = self.__class__(self.filename)
         file_copy.write(self.data)
         file_copy.seek(0)
@@ -27,11 +26,13 @@ class StringFile(io.StringIO):
     class for handling text files with name
     default encoding : utf-8
     """
+
     def __init__(self, filename: str = ''):
         super().__init__()
         self.filename = filename
 
     def copy(self):
+        """ Files deep copy  """
         file_copy = self.__class__(self.filename)
         file_copy.write(self.data)
         file_copy.seek(0)
