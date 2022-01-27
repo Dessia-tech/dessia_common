b1c3971d568e09f2f6dbde5902572cda30a5e6d2
Ghislain Jezequel <jezequel@dessia.tech>
2022-01-27 16:11:52 +0100

    feat(workflows): add public workflows

--- dessia_common/models/__init__.py
+++ dessia_common/models/__init__.py
@@ -1,2 +1,4 @@
 from .cars import catalog
 from .power_test import simulation1, simulation_list, system1
+from .workflows.wokflow_exports import workflow_export, workflow_export_state
+from .workflows.forms_workflow import workflow_, workflow_state
