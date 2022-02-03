0cdd84a4cd7ba6860fcfeb5e80b5f86431ce4862
Steven Masfaraud <masfaraud@dessia.tech>
2022-02-03 14:33:29 +0100

    fix: message in error rather than args[0]

--- dessia_common/errors.py
+++ dessia_common/errors.py
@@ -8,10 +8,11 @@
 class ExceptionWithTraceback(Exception):
     def __init__(self, message, traceback_=''):
         super().__init__(message)
+        self.message = message
         self.traceback = traceback_
 
     def __str__(self):
-        return f'{self.args[0]}\nTraceback:\n{self.traceback}'
+        return f'{self.message}\nTraceback:\n{self.traceback}'
 
 
 class DeepAttributeError(ExceptionWithTraceback, AttributeError):
