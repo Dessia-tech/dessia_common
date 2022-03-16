cda1668ba2d381a9136b4896e6b6e0ca8420c71e
Ghislain Jezequel <jezequel@dessia.tech>
2022-03-16 10:29:17 +0100

    fix(tests): fix other <3.9 errors

--- tests/type_matching.py
+++ tests/type_matching.py
@@ -13,19 +13,19 @@ assert typematch(DessiaObject, object)  # DessiaObject IS an object
 assert not typematch(object, DessiaObject)  # object COULD BE a DessiaObject but not necessarily => must raise an error
 
 # LISTS
-assert typematch(list[int], list[int])
+assert typematch(List[int], List[int])
 assert typematch(List[Measure], List[float])
 assert not typematch(List[float], List[Measure])
 assert typematch(List[StandaloneObjectWithDefaultValues], List[DessiaObject])
 assert typematch(List[StandaloneObjectWithDefaultValues], List[StandaloneObject])
 
 # TUPLES
-assert typematch(tuple[int, str], tuple[int, str])
+assert typematch(Tuple[int, str], Tuple[int, str])
 assert not typematch(Tuple[int, int], Tuple[str, int])
 
 # DEFAULT VALUES
 assert typematch(Optional[List[StandaloneObject]], List[DessiaObject])
-assert typematch(Union[list[StandaloneObject], type(None)], list[DessiaObject])
+assert typematch(Union[List[StandaloneObject], type(None)], List[DessiaObject])
 
 # UNION
 assert typematch(DessiaObject, Union[DessiaObject, int])
