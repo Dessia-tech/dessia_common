d6b0239b2d7bec29cf66fad228215d092feaea92
Steven Masfaraud <masfaraud@dessia.tech>
2022-04-22 16:57:49 +0200

    fix: evaluating subsubreference in get_in_object_from_path

--- dessia_common/breakdown.py
+++ dessia_common/breakdown.py
@@ -63,6 +63,10 @@ def get_in_object_from_path(object_, path):
     segments = path.lstrip('#/').split('/')
     element = object_
     for segment in segments:
+        if '$ref' in element:
+            # Going down in the object and it is a reference
+            # Evaluating subreference
+            element = get_in_object_from_path(object_, element['$ref'])
         element = extract_from_object(element, segment)
 
     return element
