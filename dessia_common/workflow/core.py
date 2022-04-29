4ee02bee53ed910fe6ab36919f783c6c657a2a14
Ghislain Jézéquel <jezequel@dessia.tech>
2022-04-29 14:59:09 +0200

    feat(workflow_name): remove unneeded statement

--- dessia_common/workflow/core.py
+++ dessia_common/workflow/core.py
@@ -1014,9 +1014,6 @@ class Workflow(Block):
 
         state.output_value = state.values[self.outputs[0]]
 
-        name_index = str(len(self.inputs) + 1)
-        if name is None and name_index in input_values:
-            name = input_values[name_index]
         if not name:
             timestamp = start_timestamp.strftime("%m-%d (%H:%M)")
             name = f"{self.name} @ [{timestamp}]"
