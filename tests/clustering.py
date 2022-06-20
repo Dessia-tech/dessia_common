d720f6f5d24d118f99267917484f7dee37ee9fe7
Tanguy-DES <tanguy.loreau@dessia.tech>
2022-06-20 16:57:35 +0200

    feat(cluster): fix autopep8 with autopep8 algorithm

--- tests/clustering.py
+++ tests/clustering.py
@@ -14,20 +14,19 @@ csv_cars = pkg_resources.resource_stream('dessia_common', 'models/data/cars.csv'
 
 all_cars, variables = tests.Car.from_csv(csv_cars)
 
-dbtest = cluster.ClusterResult.from_dbscan(all_cars, eps = 0.1)
-aggclustest = cluster.ClusterResult.from_agglomerative_clustering(all_cars, n_clusters = 10)
-kmeanstest = cluster.ClusterResult.from_kmeans(all_cars, n_clusters = 10)
+dbtest = cluster.ClusterResult.from_dbscan(all_cars, eps=0.1)
+aggclustest = cluster.ClusterResult.from_agglomerative_clustering(all_cars, n_clusters=10)
+kmeanstest = cluster.ClusterResult.from_kmeans(all_cars, n_clusters=10)
 
 
-db_list = dbtest.data_to_clusters(all_cars, dbtest.labels)
-# agg_list = aggclustest.data_to_clusters(all_cars, aggclustest.labels)
+# db_list = dbtest.data_to_clusters(all_cars, dbtest.labels)
+agg_list = aggclustest.data_to_clusters(all_cars, aggclustest.labels)
 # kmeans_list = kmeanstest.data_to_clusters(all_cars, kmeanstest.labels)
 
-dbtest.check_dimensionality(all_cars)
-# aggclustest.check_dimensionality(all_cars)
+# dbtest.check_dimensionality(all_cars)
+aggclustest.check_dimensionality(all_cars)
 # kmeanstest.check_dimensionality(all_cars)
 
-dbtest.plot_data()
-# aggclustest.plot_data()
+# dbtest.plot_data()
+aggclustest.plot_data()
 # kmeanstest.plot_data()
-
--- tests/files.py
+++ tests/files.py
@@ -12,6 +12,6 @@ import inspect
 
 module = importlib.import_module('dessia_common.files')
 
-for name,obj in inspect.getmembers(module):
+for name, obj in inspect.getmembers(module):
     if inspect.isclass(obj) and issubclass(obj, (io.BytesIO, io.StringIO)):
         template = obj.save_template_to_file('test')
