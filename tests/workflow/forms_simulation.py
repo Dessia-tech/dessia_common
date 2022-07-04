96fecd5f04541b9eec8415e9175e4bdd798a6519
jezequel <jezequel@dessia.tech>
2022-07-04 15:35:32 +0200

    test(wr): test new _get_from_path method

--- tests/workflow/forms_simulation.py
+++ tests/workflow/forms_simulation.py
@@ -57,3 +57,6 @@ workflow_._check_platform()
 # workflow_run._check_platform()
 
 arguments = workflow_.dict_to_arguments(input_values, 'run')
+
+# Check Breakdown
+assert workflow_run._get_from_path("#/values/1") == "Test"
