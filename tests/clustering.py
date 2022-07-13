81cfdad6d2f2ae291752757d2b25cba37df84114
Tanguy-DES <tanguy.loreau@dessia.tech>
2022-07-13 11:49:46 +0200

    fix(cluster): Change List to Tuple in Clustesters.create_dataset and add tests in clustering.py

--- tests/clustering.py
+++ tests/clustering.py
@@ -16,16 +16,16 @@ csv_cars = pkg_resources.resource_stream('dessia_common', 'models/data/cars.csv'
 all_cars_with_features = HeterogeneousList(tests.CarWithFeatures.from_csv(csv_cars))
 
 # Auto-generated heterogeneous small dataset with nb_clusters clusters of points in nb_dims dimensions
-mean_borns = [-50, 50]
-std_borns = [-2, 2]
+mean_borns = (-50, 50)
+std_borns = (-2, 2)
 small_clustesters_heterogeneous = HeterogeneousList(
     tests.ClusTester_d5.create_dataset(nb_clusters = 10, nb_points = 250, mean_borns = mean_borns, std_borns = std_borns) +
     tests.ClusTester_d4.create_dataset(nb_clusters = 10, nb_points = 250, mean_borns = mean_borns, std_borns = std_borns) +
     tests.ClusTester_d3.create_dataset(nb_clusters = 10, nb_points = 250, mean_borns = mean_borns, std_borns = std_borns))
 
 # Auto-generated heterogeneous large dataset with nb_clusters clusters of points in nb_dims dimensions
-mean_borns = [-50, 50]
-std_borns = [-2, 2]
+mean_borns = (-50, 50)
+std_borns = (-2, 2)
 big_clustesters_heterogeneous = HeterogeneousList(
     tests.ClusTester_d9.create_dataset(nb_clusters = 10, nb_points = 500, mean_borns = mean_borns, std_borns = std_borns) +
     tests.ClusTester_d7.create_dataset(nb_clusters = 10, nb_points = 500, mean_borns = mean_borns, std_borns = std_borns) +
@@ -98,13 +98,21 @@ workflow = wf.Workflow(block_workflow, pipe_worflow, block_cluster.outputs[0])
 workflow_run = workflow.run({workflow.index(block_data.inputs[0]): pkg_resources.resource_stream('dessia_common', 'models/data/cars.csv'),
                               workflow.index(block_cluster.inputs[1]): 40})
 
-ff = workflow_run.output_value.display_settings()[1]
-ff.selector
+# workflow._check_platform()
+# workflow.plot()
+# workflow.display_settings()
+# workflow_run.output_value.plot()
 
-workflow._check_platform()
-workflow.plot()
-workflow.display_settings()
-workflow_run.output_value.plot()
+# JSON TESTS
+dict_workflow = workflow.to_dict(use_pointers=True)
+json_dict = json.dumps(dict_workflow)
+decoded_json = json.loads(json_dict)
+deserialized_object = workflow.dict_to_object(decoded_json)
+
+# dict_workflow_run = workflow_run.to_dict(use_pointers=True)
+# json_dict = json.dumps(dict_workflow_run)
+# decoded_json = json.loads(json_dict)
+# deserialized_object = workflow_run.dict_to_object(decoded_json)
 
 
 # =============================================================================
@@ -126,13 +134,22 @@ workflow = wf.Workflow(block_workflow, pipe_worflow, block_cluster.outputs[0])
 workflow_run = workflow.run({workflow.index(block_data.inputs[0]): pkg_resources.resource_stream('dessia_common', 'models/data/cars.csv'),
                               workflow.index(block_cluster.inputs[1]): 40})
 
-ff = workflow_run.output_value.display_settings()[1]
-ff.selector
 
-workflow._check_platform()
-workflow.plot()
-workflow.display_settings()
-workflow_run.output_value.plot()
+# workflow._check_platform()
+# workflow.plot()
+# workflow.display_settings()
+# workflow_run.output_value.plot()
+
+# JSON TESTS
+dict_workflow = workflow.to_dict(use_pointers=True)
+json_dict = json.dumps(dict_workflow)
+decoded_json = json.loads(json_dict)
+deserialized_object = workflow.dict_to_object(decoded_json)
+
+# dict_workflow_run = workflow_run.to_dict(use_pointers=True)
+# json_dict = json.dumps(dict_workflow_run)
+# decoded_json = json.loads(json_dict)
+# deserialized_object = workflow_run.dict_to_object(decoded_json)
 
 
 # =============================================================================
@@ -173,18 +190,26 @@ workflow_run = workflow.run({workflow.index(block_data_d5.inputs[0]): 10, workfl
                              workflow.index(block_data_d3.inputs[0]): 10, workflow.index(block_data_d3.inputs[1]): 500,
                              workflow.index(block_data_d3.inputs[2]): mean_borns, workflow.index(block_data_d3.inputs[3]): std_borns,
 
-                             workflow.index(block_data_d4.inputs[1]): 40,
-                             workflow.index(block_data_d3.inputs[1]): 40})
+                             workflow.index(block_cluster.inputs[1]): 40})
 
-ff = workflow_run.output_value.display_settings()[1]
-ff.selector
 
-workflow._check_platform()
-workflow.plot()
-workflow.display_settings()
-workflow_run.output_value.plot()
 
+# workflow._check_platform()
+# workflow.plot()
+# workflow.display_settings()
+# workflow_run.output_value.plot()
+
+# JSON TESTS
+dict_workflow = workflow.to_dict(use_pointers=True)
+json_dict = json.dumps(dict_workflow)
+decoded_json = json.loads(json_dict)
+deserialized_object = workflow.dict_to_object(decoded_json)
+
+dict_workflow_run = workflow_run.to_dict(use_pointers=True)
+json_dict = json.dumps(dict_workflow_run)
+decoded_json = json.loads(json_dict)
+deserialized_object = workflow_run.dict_to_object(decoded_json)
 
 # gg = workflow_run._display_from_selector('plot_data')
-# json.dumps(gg.to_dict())
+# json.dumps(workflow_run.to_dict())
 # workflow_run._displays
