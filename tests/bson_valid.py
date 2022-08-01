6b44839654f32f0ea43bffd14e6ede6d3ddfe3bc
xavier-ferry <xavier.ferry45@gmail.com>
2022-08-01 10:07:20 +0200

    test(test): test

--- tests/bson_valid.py
+++ tests/bson_valid.py
@@ -23,7 +23,7 @@ class TestBsonValid(unittest.TestCase):
 
     def test_bson_valid(self):
         valid, hint = is_bson_valid(self.value, allow_nonstring_keys=True)
-        print(hint)
+        print("toto", hint)
         self.assertEqual(valid, self.expected_result)
 
 
