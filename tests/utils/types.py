cc50d86956cd6465db46a5ab1e31683be1dff9f9
xavier-ferry <xavier.ferry45@gmail.com>
2022-08-01 15:16:59 +0200

    reduce tests to specific Type

--- tests/utils/types.py
+++ tests/utils/types.py
@@ -4,132 +4,14 @@ from typing import List, Tuple, Type, Dict
 from dessia_common.forms import StandaloneObject
 
 # === Typing Serialization ===
-# Sequences
-test_typing = List[StandaloneObject]
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'List[dessia_common.forms.StandaloneObject]'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
-
-test_typing = List[int]
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'List[__builtins__.int]'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
-
-test_typing = Tuple[int, StandaloneObject]
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'Tuple[__builtins__.int, dessia_common.forms.StandaloneObject]'
-try:
-    deserialized_typing = deserialize_typing(serialized_typing)
-except TypeError:
-    # Workflow cannot have heterogeneous tuples for non_block_variables ???
-    pass
-
-test_typing = Tuple[StandaloneObject, StandaloneObject]
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'Tuple[dessia_common.forms.StandaloneObject, dessia_common.forms.StandaloneObject]'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == Tuple[StandaloneObject, ...]
-
-# test_typing = collections.abc.Iterator[StandaloneObject]
-# serialized_typing = serialize_typing(test_typing)
-# assert serialized_typing == 'Iterator[dessia_common.forms.StandaloneObject]'
-# deserialized_typing = deserialize_typing(serialized_typing)
-# assert deserialized_typing == test_typing
-
-# Nested sequences
-test_typing = List[List[StandaloneObject]]
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'List[List[dessia_common.forms.StandaloneObject]]'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
-
-test_typing = List[List[List[StandaloneObject]]]
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'List[List[List[dessia_common.forms.StandaloneObject]]]'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
-
-# test_typing = Tuple[Tuple[int, StandaloneObject]]
-# serialized_typing = serialize_typing(test_typing)
-# assert serialized_typing == 'Tuple[Tuple[__builtins__.int, dessia_common.forms.StandaloneObject]]'
-# deserialized_typing = deserialize_typing(serialized_typing)
-# assert deserialized_typing == test_typing
-#
-# test_typing = Tuple[Tuple[StandaloneObject, StandaloneObject]]
-# serialized_typing = serialize_typing(test_typing)
-# assert serialized_typing == 'Tuple[Tuple[dessia_common.forms.StandaloneObject, dessia_common.forms.StandaloneObject]]'
-# deserialized_typing = deserialize_typing(serialized_typing)
-# assert deserialized_typing == test_typing
-
-# Dictionnaries
-test_typing = Dict[int, StandaloneObject]
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'Dict[__builtins__.int, dessia_common.forms.StandaloneObject]'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
-
-# test_typing = Dict[str, Dict[int, StandaloneObject]]
-# serialized_typing = serialize_typing(test_typing)
-# assert serialized_typing == 'Dict[__builtins__.str, Dict[__builtins__.int, dessia_common.forms.StandaloneObject]]'
-# deserialized_typing = deserialize_typing(serialized_typing)
-# assert deserialized_typing == test_typing
-
 # Types
+print("------------- Testing Serialization :")
 test_typing = Type
 serialized_typing = serialize_typing(test_typing)
 assert serialized_typing == 'typing.Type'
+print("ok")
+
+print("------------- Testing deserialization : ")
 deserialized_typing = deserialize_typing(serialized_typing)
 assert deserialized_typing == test_typing
-
-test_typing = Type[StandaloneObject]
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'Type'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert not deserialized_typing == test_typing
-
-test_typing = InstanceOf[StandaloneObject]
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'InstanceOf[dessia_common.forms.StandaloneObject]'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
-
-# test_typing = Subclass[StandaloneObject]
-# serialized_typing = serialize_typing(test_typing)
-# assert serialized_typing == 'InstanceOf[dessia_common.forms.StandaloneObject]'
-# deserialized_typing = deserialize_typing(serialized_typing)
-# assert deserialized_typing == test_typing
-
-test_typing = MethodType
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'MethodType'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
-
-test_typing = ClassMethodType
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'ClassMethodType'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
-
-# test_typing = Union[StandaloneObject, str, int]
-# serialized_typing = serialize_typing(test_typing)
-# assert serialized_typing == 'Union[dessia_common.forms.StandaloneObject, __builtins__.str, __builtins__.int]'
-# deserialized_typing = deserialize_typing(serialized_typing)
-# assert deserialized_typing == test_typing
-
-# Files
-test_typing = StringFile
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'StringFile'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
-
-test_typing = BinaryFile
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'BinaryFile'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
-
-print("TYPING SERIALISATION TESTS PASSED")
+print("ok")
