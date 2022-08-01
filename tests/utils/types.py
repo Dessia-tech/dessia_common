fe7901957ccdc4425b02dfe55c9946233f5b5b73
xavier-ferry <xavier.ferry45@gmail.com>
2022-08-01 13:41:16 +0200

    Revert "test(test): remove unittest in types.py"
    This reverts commit e53ab3123eb73c81df3935071ef8060e932a104e.

--- tests/utils/types.py
+++ tests/utils/types.py
@@ -1,3 +1,7 @@
+import unittest
+
+from parameterized import parameterized_class
+
 from dessia_common.utils.types import deserialize_typing, serialize_typing, MethodType, ClassMethodType, InstanceOf
 from dessia_common.files import BinaryFile, StringFile
 from typing import List, Tuple, Type, Dict
@@ -5,18 +9,6 @@ from dessia_common.forms import StandaloneObject
 
 # === Typing Serialization ===
 # Sequences
-test_typing = List[StandaloneObject]
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'List[dessia_common.forms.StandaloneObject]'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
-
-test_typing = List[int]
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'List[__builtins__.int]'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
-
 test_typing = Tuple[int, StandaloneObject]
 serialized_typing = serialize_typing(test_typing)
 assert serialized_typing == 'Tuple[__builtins__.int, dessia_common.forms.StandaloneObject]'
@@ -39,17 +31,6 @@ assert deserialized_typing == Tuple[StandaloneObject, ...]
 # assert deserialized_typing == test_typing
 
 # Nested sequences
-test_typing = List[List[StandaloneObject]]
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'List[List[dessia_common.forms.StandaloneObject]]'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
-
-test_typing = List[List[List[StandaloneObject]]]
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'List[List[List[dessia_common.forms.StandaloneObject]]]'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
 
 # test_typing = Tuple[Tuple[int, StandaloneObject]]
 # serialized_typing = serialize_typing(test_typing)
@@ -64,11 +45,6 @@ assert deserialized_typing == test_typing
 # assert deserialized_typing == test_typing
 
 # Dictionnaries
-test_typing = Dict[int, StandaloneObject]
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'Dict[__builtins__.int, dessia_common.forms.StandaloneObject]'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
 
 # test_typing = Dict[str, Dict[int, StandaloneObject]]
 # serialized_typing = serialize_typing(test_typing)
@@ -77,23 +53,11 @@ assert deserialized_typing == test_typing
 # assert deserialized_typing == test_typing
 
 # Types
-test_typing = Type
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'typing.Type'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
-
 test_typing = Type[StandaloneObject]
 serialized_typing = serialize_typing(test_typing)
 assert serialized_typing == 'Type'
 deserialized_typing = deserialize_typing(serialized_typing)
 assert not deserialized_typing == test_typing
-
-test_typing = InstanceOf[StandaloneObject]
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'InstanceOf[dessia_common.forms.StandaloneObject]'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
 
 # test_typing = Subclass[StandaloneObject]
 # serialized_typing = serialize_typing(test_typing)
@@ -101,139 +65,37 @@ assert deserialized_typing == test_typing
 # deserialized_typing = deserialize_typing(serialized_typing)
 # assert deserialized_typing == test_typing
 
-test_typing = MethodType
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'MethodType'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
-
-test_typing = ClassMethodType
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'ClassMethodType'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
-
 # test_typing = Union[StandaloneObject, str, int]
 # serialized_typing = serialize_typing(test_typing)
 # assert serialized_typing == 'Union[dessia_common.forms.StandaloneObject, __builtins__.str, __builtins__.int]'
 # deserialized_typing = deserialize_typing(serialized_typing)
 # assert deserialized_typing == test_typing
 
-# Files
-test_typing = StringFile
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'StringFile'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
+print("TYPING SERIALISATION ASSERTS PASSED")
 
-test_typing = BinaryFile
-serialized_typing = serialize_typing(test_typing)
-assert serialized_typing == 'BinaryFile'
-deserialized_typing = deserialize_typing(serialized_typing)
-assert deserialized_typing == test_typing
 
-print("TYPING SERIALISATION TESTS PASSED")
+# Some special cases above still use asserts
+@parameterized_class(('value', 'expected_serialized'), [
+    (List[StandaloneObject], 'List[dessia_common.forms.StandaloneObject]'),
+    (List[int], 'List[__builtins__.int]'),
+    # Nested Sequence
+    (List[List[StandaloneObject]], 'List[List[dessia_common.forms.StandaloneObject]]'),
+    (List[List[List[StandaloneObject]]], 'List[List[List[dessia_common.forms.StandaloneObject]]]'),
 
-#
-#
-# import unittest
-#
-# from parameterized import parameterized_class
-#
-# from dessia_common.utils.types import deserialize_typing, serialize_typing, MethodType, ClassMethodType, InstanceOf
-# from dessia_common.files import BinaryFile, StringFile
-# from typing import List, Tuple, Type, Dict
-# from dessia_common.forms import StandaloneObject
-#
-# # === Typing Serialization ===
-# # Sequences
-# test_typing = Tuple[int, StandaloneObject]
-# serialized_typing = serialize_typing(test_typing)
-# assert serialized_typing == 'Tuple[__builtins__.int, dessia_common.forms.StandaloneObject]'
-# try:
-#     deserialized_typing = deserialize_typing(serialized_typing)
-# except TypeError:
-#     # Workflow cannot have heterogeneous tuples for non_block_variables ???
-#     pass
-#
-# test_typing = Tuple[StandaloneObject, StandaloneObject]
-# serialized_typing = serialize_typing(test_typing)
-# assert serialized_typing == 'Tuple[dessia_common.forms.StandaloneObject, dessia_common.forms.StandaloneObject]'
-# deserialized_typing = deserialize_typing(serialized_typing)
-# assert deserialized_typing == Tuple[StandaloneObject, ...]
-#
-# # test_typing = collections.abc.Iterator[StandaloneObject]
-# # serialized_typing = serialize_typing(test_typing)
-# # assert serialized_typing == 'Iterator[dessia_common.forms.StandaloneObject]'
-# # deserialized_typing = deserialize_typing(serialized_typing)
-# # assert deserialized_typing == test_typing
-#
-# # Nested sequences
-#
-# # test_typing = Tuple[Tuple[int, StandaloneObject]]
-# # serialized_typing = serialize_typing(test_typing)
-# # assert serialized_typing == 'Tuple[Tuple[__builtins__.int, dessia_common.forms.StandaloneObject]]'
-# # deserialized_typing = deserialize_typing(serialized_typing)
-# # assert deserialized_typing == test_typing
-# #
-# # test_typing = Tuple[Tuple[StandaloneObject, StandaloneObject]]
-# # serialized_typing = serialize_typing(test_typing)
-# # assert serialized_typing == 'Tuple[Tuple[dessia_common.forms.StandaloneObject, dessia_common.forms.StandaloneObject]]'
-# # deserialized_typing = deserialize_typing(serialized_typing)
-# # assert deserialized_typing == test_typing
-#
-# # Dictionnaries
-#
-# # test_typing = Dict[str, Dict[int, StandaloneObject]]
-# # serialized_typing = serialize_typing(test_typing)
-# # assert serialized_typing == 'Dict[__builtins__.str, Dict[__builtins__.int, dessia_common.forms.StandaloneObject]]'
-# # deserialized_typing = deserialize_typing(serialized_typing)
-# # assert deserialized_typing == test_typing
-#
-# # Types
-# test_typing = Type[StandaloneObject]
-# serialized_typing = serialize_typing(test_typing)
-# assert serialized_typing == 'Type'
-# deserialized_typing = deserialize_typing(serialized_typing)
-# assert not deserialized_typing == test_typing
-#
-# # test_typing = Subclass[StandaloneObject]
-# # serialized_typing = serialize_typing(test_typing)
-# # assert serialized_typing == 'InstanceOf[dessia_common.forms.StandaloneObject]'
-# # deserialized_typing = deserialize_typing(serialized_typing)
-# # assert deserialized_typing == test_typing
-#
-# # test_typing = Union[StandaloneObject, str, int]
-# # serialized_typing = serialize_typing(test_typing)
-# # assert serialized_typing == 'Union[dessia_common.forms.StandaloneObject, __builtins__.str, __builtins__.int]'
-# # deserialized_typing = deserialize_typing(serialized_typing)
-# # assert deserialized_typing == test_typing
-#
-# print("TYPING SERIALISATION ASSERTS PASSED")
-#
-#
-# # Some special cases above still use asserts
-# @parameterized_class(('value', 'expected_serialized'), [
-#     (List[StandaloneObject], 'List[dessia_common.forms.StandaloneObject]'),
-#     (List[int], 'List[__builtins__.int]'),
-#     # Nested Sequence
-#     (List[List[StandaloneObject]], 'List[List[dessia_common.forms.StandaloneObject]]'),
-#     (List[List[List[StandaloneObject]]], 'List[List[List[dessia_common.forms.StandaloneObject]]]'),
-#
-#     # Dictionnaries
-#     (Dict[int, StandaloneObject], 'Dict[__builtins__.int, dessia_common.forms.StandaloneObject]'),
-#     # Types
-#     (Type, 'typing.Type'),
-#     (InstanceOf[StandaloneObject], 'InstanceOf[dessia_common.forms.StandaloneObject]'),
-#     (MethodType, 'dessia_common.typings.MethodType'),
-#     (ClassMethodType, 'dessia_common.typings.ClassMethodType'),
-#     # Files
-#     (StringFile, 'dessia_common.files.StringFile'),
-#     (BinaryFile, 'dessia_common.files.BinaryFile')
-# ])
-# class TestTypingSerializationValid(unittest.TestCase):
-#
-#     def test_serialization(self):
-#         serialized = serialize_typing(self.value)
-#         self.assertEqual(serialized, self.expected_serialized)
-#         self.assertEqual(self.value, deserialize_typing(serialized))
+    # Dictionnaries
+    (Dict[int, StandaloneObject], 'Dict[__builtins__.int, dessia_common.forms.StandaloneObject]'),
+    # Types
+    (Type, 'typing.Type'),
+    (InstanceOf[StandaloneObject], 'InstanceOf[dessia_common.forms.StandaloneObject]'),
+    (MethodType, 'dessia_common.typings.MethodType'),
+    (ClassMethodType, 'dessia_common.typings.ClassMethodType'),
+    # Files
+    (StringFile, 'dessia_common.files.StringFile'),
+    (BinaryFile, 'dessia_common.files.BinaryFile')
+])
+class TestTypingSerializationValid(unittest.TestCase):
+
+    def test_serialization(self):
+        serialized = serialize_typing(self.value)
+        self.assertEqual(serialized, self.expected_serialized)
+        self.assertEqual(self.value, deserialize_typing(serialized))
