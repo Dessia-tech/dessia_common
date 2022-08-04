f399512b24b383dfc20e2dde4cd71d0677391f16
Tanguy-DES <tanguy.loreau@dessia.tech>
2022-08-04 15:12:40 +0200

    fix(HL_filters): Change _procreate to solve bug

--- dessia_common/core.py
+++ dessia_common/core.py
@@ -927,7 +927,8 @@ class HeterogeneousList(DessiaObject):
     def _procreate(self):
         new_hlist = self.__class__()
         new_hlist.name = self.name
-        new_hlist._common_attributes = self._common_attributes
+        new_hlist._common_attributes = self.common_attributes
+        new_hlist._matrix = self.matrix
         return new_hlist
 
     def __getitem__(self, key: Any):
