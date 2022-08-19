9291a0f4fb9cb18895817b1c446664b92023debf
Tanguy-DES <tanguy.loreau@dessia.tech>
2022-08-19 17:45:03 +0200

    feat(HL_docstrings): Improve coverage note at coverage.py commands

--- tests/coverage.py
+++ tests/coverage.py
@@ -9,7 +9,7 @@ Created on Wed Sep 29 14:35:47 2021
 import json
 
 MIN_MODULE_COVERAGE = 51.
-MIN_GLOBAL_COVERAGE = 78.4
+MIN_GLOBAL_COVERAGE = 81.5
 RATCHET_COVERAGE = 3.
 
 untracked_modules = ["dessia_common/templates.py",
