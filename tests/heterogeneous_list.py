fd85a317c1825a20c0b78e4988996ff86762aa7f
Tanguy-DES <tanguy.loreau@dessia.tech>
2022-08-31 10:27:31 +0200

    Revert "feat(HList_docstrings): Remove _procreate method"
    This reverts commit 2f61eeb28986a529cfb7c314e3ba4dc4f6271111.

--- tests/heterogeneous_list.py
+++ tests/heterogeneous_list.py
@@ -41,8 +41,8 @@ assert(all(item in all_cars_without_features.matrix[idx]
 
 # Tests for displays
 all_cars_with_features.plot()
-all_cars_without_features.plot()
-RandData_heterogeneous.plot()
+# all_cars_without_features.plot()
+# RandData_heterogeneous.plot()
 print(all_cars_with_features)
 
 # Tests for empty HeterogeneousList
