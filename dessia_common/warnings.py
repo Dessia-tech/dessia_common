62f5bcda2df4d6ad004775de1f4f680b0afddb3e
Ghislain Jézéquel <jezequel@dessia.tech>
2022-09-06 10:36:41 +0200

    feat: add warnings module

---
+++ dessia_common/warnings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module that stores warnings in dessia_common
+"""
+
+import warnings
+
+
+class SerializationWarning(Warning):
+    def __init__(self, message: str):
+        self.message = message
+
+        Warning.__init__(self, message)
