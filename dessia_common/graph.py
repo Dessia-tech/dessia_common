c9e71b420518d57fa5c18f455c7928edcb1180ed
Steven Masfaraud <masfaraud@dessia.tech>
2022-09-20 18:54:55 +0200

    perf: better algorithm to explore a graph from its leaves

--- dessia_common/graph.py
+++ dessia_common/graph.py
@@ -6,7 +6,75 @@ Created on Thu Nov 18 14:16:30 2021
 @author: steven
 """
 
+# import random
 import networkx as nx
+
+
+class GraphExplorer:
+
+    def __init__(self, graph: nx.DiGraph):
+        self.graph = graph
+        self.exploration_order = []
+        self.number_nodes = self.graph.number_of_nodes()
+        self.successors = {}
+
+        self.remaining_nodes = list(self.graph.nodes)
+        self.explored = {n: False for n in self.remaining_nodes}
+
+    def are_neighbors_explored(self, node):
+        try:
+            node_successors = self.successors[node]
+        except KeyError:
+            node_successors = list(self.graph.successors(node))
+            self.successors[node] = node_successors
+
+        for out_node in node_successors:
+            if not self.explored[out_node]:
+                return False
+        return True
+
+    def add_node_to_order(self, node):
+        self.explored[node] = True
+        self.number_nodes -= 1
+        self.exploration_order.append(node)
+        self.remaining_nodes.remove(node)
+
+    def add_node_to_order_with_index(self, node, index=None):
+        self.explored[node] = True
+        self.number_nodes -= 1
+        self.exploration_order.append(node)
+        del self.remaining_nodes[index]
+
+    def explore_tree_from_leaves(self):
+        while self.number_nodes:
+            found_node = False
+            # Regular find of a node
+            for node_index, node in enumerate(self.remaining_nodes):
+                if self.are_neighbors_explored(node):
+                    # Mark explored
+                    self.add_node_to_order_with_index(node, node_index)
+                    found_node = True
+                    current_node = node
+                    break
+
+            if found_node:
+                parent_found = True
+                while parent_found:
+                    # Searching if parents are unlocked
+                    parent_found = False
+                    for parent in self.graph.predecessors(current_node):
+                        if self.are_neighbors_explored(parent):
+                            self.add_node_to_order(parent)
+                            current_node = parent
+                            parent_found = True
+
+            else:
+                raise ValueError('Can not find a node')
+        return self.exploration_order
+
+
+def explore_tree_from_leaves(graph):
+    return GraphExplorer(graph).explore_tree_from_leaves()
 
 
 def cut_tree_final_branches(graph: nx.DiGraph):
@@ -30,43 +98,6 @@ def cut_tree_final_branches(graph: nx.DiGraph):
             graph = nx.subgraph(graph, new_nodes)
 
     return graph
-
-
-def explore_tree_from_leaves(graph: nx.DiGraph):
-    exploration_order = []
-    explored = {n: False for n in graph.nodes}
-    number_nodes = graph.number_of_nodes()
-    successors = {}
-
-    # ns = 0
-    while number_nodes:
-        found_node = False
-        # Finding a starting node
-        for node in graph.nodes:
-            if not explored[node]:
-                neighbors_explored = True
-                if node in successors:
-                    node_successors = successors[node]
-                else:
-                    node_successors = list(graph.successors(node))
-                    successors[node] = node_successors
-                    # ns += 1
-
-                for out_node in node_successors:
-                    if not explored[out_node]:
-                        neighbors_explored = False
-                        break
-
-                if neighbors_explored:
-                    # Mark explored
-                    explored[node] = True
-                    exploration_order.append(node)
-                    number_nodes -= 1
-                    found_node = True
-                    break
-        if not found_node:
-            raise ValueError('Can not find a node')
-    return exploration_order
 
 
 def extract_region(networkx_graph: nx.Graph, nodes, distance: int = 5):
--- dessia_common/utils/serialization.py
+++ dessia_common/utils/serialization.py
@@ -426,8 +426,9 @@ def pointer_graph(value):
 
     # refs = []
     refs_by_path = {}
+    references = find_references(value)
 
-    for path, reference in find_references(value):
+    for path, reference in references:
         # refs.append(reference)
         refs_by_path[path] = reference
         # Slitting path & reference to add missing nodes
@@ -458,7 +459,6 @@ def pointer_graph(value):
     graph.add_edges_from(path_edges, path=True)
     graph.add_edges_from(pointer_edges, head_type='circle', color='#008000', path=False)
 
-    references = find_references(value)
     refs_by_path = {r[0]: r[1] for r in references}
     extra_edges = []
     new_paths = {}
