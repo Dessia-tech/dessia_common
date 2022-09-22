30e9dee8be31df2d0521fa566c0d6c5a44bd1af4
Ghislain Jézéquel <jezequel@dessia.tech>
2022-09-22 16:07:18 +0200

    feat(exports): add filename as archive inputs

--- dessia_common/workflow/core.py
+++ dessia_common/workflow/core.py
@@ -1984,6 +1984,7 @@ class WorkflowState(DessiaObject):
             stream.write(export_stream.getvalue())
         if isinstance(stream, BinaryFile):
             stream.write(export_stream.getbuffer())
+        stream.filename = export_stream.filename
         return export_stream
 
 
