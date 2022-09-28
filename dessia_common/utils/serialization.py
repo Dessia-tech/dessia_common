77c4c423f18e2626135357ebbd559237df9c3c65
Tanguy-DES <tanguy.loreau@dessia.tech>
2022-09-28 16:03:59 +0200

    feat(sampling): Move error meassge: see if entering in condition

--- dessia_common/utils/serialization.py
+++ dessia_common/utils/serialization.py
@@ -359,8 +359,9 @@ def deserialize_argument(type_, argument):
     """
     if argument is None:
         return None
-    raise NotImplementedError(f'{type_}, {dcty.Type}, {type_ == dcty.Type}, {argument}')
+
     if type_ == dcty.Type:
+        raise NotImplementedError(f'{type_}, {dcty.Type}, {type_ == dcty.Type}, {argument}')
         return dcty.is_classname_transform(argument)
 
     if dcty.is_typing(type_):
