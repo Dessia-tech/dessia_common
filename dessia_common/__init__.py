4d8b5c44fde648ca45cc49f37e8d6f3742f1cbcd
Tanguy-DES <tanguy.loreau@dessia.tech>
2022-10-11 16:50:11 +0200

    fix(hlist_rename): New try for import

--- dessia_common/__init__.py
+++ dessia_common/__init__.py
@@ -6,7 +6,7 @@ __init__ for dessia_common module
 
 import pkg_resources
 from .core import *
-from dessia_common import datatools
+from . import datatools
 
 __version__ = pkg_resources.require("dessia_common")[0].version
 
