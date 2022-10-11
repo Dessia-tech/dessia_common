4268a5043e25ff3dd8c38bc1dde6790549d191f2
Tanguy-DES <tanguy.loreau@dessia.tech>
2022-10-11 16:30:00 +0200

    fix(hlist_rename): Try import datatools in init dessia_common

--- dessia_common/__init__.py
+++ dessia_common/__init__.py
@@ -6,6 +6,7 @@ __init__ for dessia_common module
 
 import pkg_resources
 from .core import *
+from dessia_common import datatools
 
 __version__ = pkg_resources.require("dessia_common")[0].version
 
