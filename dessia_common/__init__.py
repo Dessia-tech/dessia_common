b9ec40a5726a8dc29fdd2cfc16338d634f7e77e9
Tanguy-DES <tanguy.loreau@dessia.tech>
2022-10-11 16:52:45 +0200

    fix(hlist_rename): How to correctly import ?

--- dessia_common/__init__.py
+++ dessia_common/__init__.py
@@ -6,7 +6,7 @@ __init__ for dessia_common module
 
 import pkg_resources
 from .core import *
-from . import datatools
+import dessia_common.datatools
 
 __version__ = pkg_resources.require("dessia_common")[0].version
 
