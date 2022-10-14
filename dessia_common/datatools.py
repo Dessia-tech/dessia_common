1676021c54ae6110ae0a57336913f3f6d2202e4a
Tanguy-DES <tanguy.loreau@dessia.tech>
2022-10-14 10:08:46 +0200

    feat(markdown): Remove fontsize

--- dessia_common/datatools.py
+++ dessia_common/datatools.py
@@ -303,8 +303,7 @@ class HeterogeneousList(DessiaObject):
 
     def _markdown_attr_table(self):
         print_limit = 10
-        table_attributes = '<font size= “1”>'
-        table_attributes += self._markdown_titles() + self._markdown_empty_row()
+        table_attributes = self._markdown_titles() + self._markdown_empty_row()
         table_attributes += self._markdown_filling(slice(0, print_limit, 1))
 
         if len(self) > 2*print_limit:
@@ -315,7 +314,7 @@ class HeterogeneousList(DessiaObject):
         if len(self) > print_limit:
             table_attributes += self._markdown_filling(slice(-print_limit, len(self)))
 
-        return table_attributes + '</font>'
+        return table_attributes
 
     def __len__(self):
         """
