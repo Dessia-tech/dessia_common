bc04f6c412a8ed12970c7533b3a13c45eab14fa5
Tanguy-DES <tanguy.loreau@dessia.tech>
2022-11-08 15:06:20 +0100

    feat(machine_learning): nice scalers not serialized but working on platform

--- dessia_common/datatools/modeler.py
+++ dessia_common/datatools/modeler.py
@@ -17,28 +17,59 @@ try:
     from plot_data.colors import BLUE, GREY
 except ImportError:
     pass
-from dessia_common.core import DessiaObject, DessiaFilter, FiltersList, templates
-from dessia_common.datatools.metrics import mean, std, variance, covariance_matrix
+from dessia_common.core import DessiaObject
 
 
-class IdentityScaler(preprocessing.StandardScaler):
-    def __init__(self, copy_matrix: bool = True):
-        preprocessing.StandardScaler.__init__(self, with_mean=False, with_std=False, copy=copy_matrix)
+class IdentityScaler(DessiaObject):
+    def __init__(self, name: str = ''):
+        DessiaObject.__init__(self, name=name)
 
-    def fit(self, matrix: List[List[float]]):
-        return
+    def _instantiate(self):
+        return preprocessing.StandardScaler(with_mean = False, with_std = False)
 
-    def fit_transform(self, matrix: List[List[float]]):
-        return matrix
+    @classmethod
+    def fit(cls, matrix: List[List[float]], name: str = ''):
+        return cls(name)
 
     def transform(self, matrix: List[List[float]]):
         return matrix
 
-    def inverse_transform(self, matrix: List[List[float]]):
-        return matrix
+    @classmethod
+    def fit_transform(cls, matrix: List[List[float]], name: str = ''):
+        return cls(name), matrix
 
 
-class BaseModel(DessiaObject):
+class StandardScaler(IdentityScaler):
+    def __init__(self, mean_: List[float] = None, scale_: List[float] = None, var_: List[float] = None, name: str = ''):
+        self.mean_ = mean_
+        self.scale_ = scale_
+        self.var_ = var_
+        IdentityScaler.__init__(self, name=name)
+
+    def _instantiate(self):
+        scaler = preprocessing.StandardScaler()
+        scaler.mean_ = self.mean_
+        scaler.scale_ = self.scale_
+        scaler.std_ = self.std_
+        return scaler
+
+    @classmethod
+    def fit(cls, matrix: List[List[float]], name: str = ''):
+        scaler = preprocessing.StandardScaler()
+        scaler.fit(matrix)
+        return cls(scaler.mean_, scaler.scale_, scaler.var_, name)
+
+    def transform(self, matrix: List[List[float]]):
+        scaler = self._instantiate()
+        return scaler.transform(matrix)
+
+    @classmethod
+    def fit_transform(cls, matrix: List[List[float]], name: str = ''):
+        scaler = cls.fit(matrix, name)
+        return cls(scaler.mean_, scaler.scale_, scaler.var_, name), scaler.transform(matrix)
+
+
+class DessiaModel(DessiaObject):
     def __init__(self, name: str = ''):
         DessiaObject.__init__(self, name=name)
 
@@ -56,29 +87,16 @@ class Modeler(DessiaObject):
     def __init__(self, scaled_inputs: bool = True, scaled_outputs: bool = False, name: str = ''):
         self.model_ = dict()
         self.model_attributes = dict()
+        self._required_attributes = []
         self.scaled_inputs = scaled_inputs
         self.scaled_outputs = scaled_outputs
         DessiaObject.__init__(self, name=name)
 
 ##### SCALE ##########
-    def _init_scaler(self, is_scaled: bool):
+    def _initialize_scaler(self, is_scaled: bool):
         if is_scaled:
             return preprocessing.StandardScaler()
         return IdentityScaler()
-
-    def _fit_scaler(self, matrix: List[List[float]], scaler):
-        scaler.fit(matrix)
-
-    def _scale_matrix(self, matrix: List[List[float]], scaler):
-        return scaler.transform(matrix)
-
-    def _fit_transform_scaler(self, matrix: List[List[float]], scaler):
-        return scaler.fit_transform(matrix)
-
-    def _auto_scale(self, matrix: List[List[float]], is_scaled: bool):
-        scaler = self._init_scaler(is_scaled)
-        scaled_matrix = self._fit_transform_scaler(matrix, scaler)
-        return scaler, scaled_matrix
 
 ##### MODEL ##########
     def _init_model(self):
@@ -99,16 +117,22 @@ class Modeler(DessiaObject):
     def fit(self, inputs: List[List[float]], outputs: List[List[float]]):
         input_scaler, scaled_inputs = self._auto_scale(inputs, self.scaled_inputs)
         output_scaler, scaled_outputs = self._auto_scale(outputs, self.scaled_outputs)
-        return self.model.fit(scaled_inputs, scaled_outputs)
+        model = self._instantiate_model()
+        model.fit(scaled_inputs, scaled_outputs)
+        self.model_ = {key: value for key, value in model.items() if key in self._required_attributes}
 
     def predict(self, inputs: List[List[float]], input_scaler, output_scaler):
         scaled_inputs = input_scaler.transform(inputs)
-        return output_scaler.inverse_transform(self.model.predict(scaled_inputs))
+        model = self._instantiate_model()
+        return output_scaler.inverse_transform(model.predict(scaled_inputs))
 
     def fit_predict(self, inputs: List[List[float]], outputs: List[List[float]]):
         input_scaler, scaled_inputs = self._auto_scale(inputs, self.scaled_inputs)
         output_scaler, scaled_outputs = self._auto_scale(outputs, self.scaled_outputs)
-        return self.model.fit_predit(scaled_inputs, scaled_outputs)
+        model = self._instantiate_model()
+        predicted_outputs = model.fit_predict(scaled_inputs, scaled_outputs)
+        self.model_ = {key: value for key, value in model.items() if key in self._required_attributes}
+        return predicted_outputs
 
 # Does sklearn objects have to be serialized ?
 
