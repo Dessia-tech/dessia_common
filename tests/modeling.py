4d33ce92376e28a626b875f228f602cf61ed854b
Tanguy-DES <tanguy.loreau@dessia.tech>
2022-11-10 18:15:12 +0100

    feat(machine_learning): change file name and add test file

---
+++ tests/modeling.py
@@ -0,0 +1,11 @@
+"""
+Modeling.py package testing.
+
+"""
+import json
+from dessia_common.models import all_cars_no_feat, all_cars_wi_feat, rand_data_small, rand_data_large
+from dessia_common.datatools import HeterogeneousList, CategorizedList
+from dessia_common.datatools.dataset import Dataset
+from dessia_common.datatools.cluster import ClusteredDataset
+
+
