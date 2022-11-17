3ed84801b42fc2b5634f20a7e0a47c2f2fbffbba
Steven Masfaraud <masfaraud@dessia.tech>
2022-11-17 15:07:31 +0100

    tests: adding tests

--- tests/checks.py
+++ tests/checks.py
@@ -4,29 +4,35 @@ import dessia_common.checks as checks
 
 
 class Battery(DessiaObject):
-    
+
     def __init__(self, capacity: int, max_dc: int,
-                  name: str = ''):
-        
+                 name: str = ''):
+
         DessiaObject.__init__(self, name=name)
         self.capacity = capacity
         self.max_dc = max_dc
-        
+
     # def is_valid(self):
     #     if self.max_dc < 50:
     #         return False
     #     return True
-    
+
     def is_valid(self):
         if checks.is_float(self.max_dc) or checks.is_float(self.max_dc):
             return False
         return True
 
+
+battery = Battery(3, 2)
+battery.checks()
+battery.is_valid()
+
+
 class ElectricChar2(DessiaObject):
-    
+
     def __init__(self, battery: Battery, brand: str, model: str, price: int, autonomy: int,
-                  name: str = ''):
-        
+                 name: str = ''):
+
         DessiaObject.__init__(self, name=name)
         self.battery = battery
         self.brand = brand
