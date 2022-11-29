1f1d5f33651603a720306c6b3060b448cab059bc
Ghislain Jézéquel <jezequel@dessia.tech>
2022-11-29 15:47:24 +0100

    fix(jsonschema): fix measures unit

--- dessia_common/utils/jsonschema.py
+++ dessia_common/utils/jsonschema.py
@@ -220,7 +220,7 @@ def jsonschema_from_annotation(annotation, jsonschema_element, order, editable=N
         ann = (key, float)
         jsonschema_element = jsonschema_from_annotation(annotation=ann, jsonschema_element=jsonschema_element,
                                                         order=order, editable=editable, title=title)
-        jsonschema_element[key]['units'] = typing_.units
+        jsonschema_element[key]['units'] = typing_.si_unit
     elif inspect.isclass(typing_) and issubclass(typing_, (BinaryFile, StringFile)):
         jsonschema_element[key].update({'type': 'text', 'is_file': True})
     else:
