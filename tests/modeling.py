7a07a08bb153a55149edc5f3e89f1939d59a9efa
Tanguy-DES <tanguy.loreau@dessia.tech>
2022-11-30 17:34:30 +0100

    feat(machine_learning): better code organisation in test file

--- tests/modeling.py
+++ tests/modeling.py
@@ -11,21 +11,24 @@ from dessia_common.datatools.modeling import StandardScaler, IdentityScaler, Lin
     DecisionTreeRegressor, DecisionTreeClassifier, RandomForestRegressor, RandomForestClassifier, MLPClassifier,\
     BaseScaler, BaseModel, BaseTree, RandomForest, SVM, MLP
 
+
 # Load Data and put it in a Dataset (matrix is automatically computed)
 dataset_example = Dataset(all_cars_no_feat)
+inputs = dataset_example.sub_matrix(['displacement', 'horsepower', 'model', 'acceleration', 'cylinders'])
+double_outputs = dataset_example.sub_matrix(['mpg', 'weight'])
+labelled_outputs = [npy.random.randint(4) for _ in double_outputs]
+mono_outputs = [output[0] for output in double_outputs]
+
 
 # Test scalers
 idty_scaler = IdentityScaler().fit(dataset_example.matrix)
 idty_matrix = idty_scaler.transform(dataset_example.matrix)
 idty_scaler, idty_matrix = IdentityScaler().fit_transform(dataset_example.matrix)
 
-inputs = dataset_example.sub_matrix(['displacement', 'horsepower', 'model', 'acceleration', 'cylinders'])
-double_outputs = dataset_example.sub_matrix(['mpg', 'weight'])
 std_scaler = StandardScaler().fit(inputs)
 std_inputs = std_scaler.transform(inputs)
 std_scaler, std_inputs = StandardScaler().fit_transform(inputs)
-labelled_outputs = [npy.random.randint(4) for _ in double_outputs]
-mono_outputs = [output[0] for output in double_outputs]
+
 
 # Hyperparameters
 ridge_hyperparams = {'alpha': 0.1, 'tol': 0.00001, 'fit_intercept': True}
@@ -34,11 +37,13 @@ rf_hyperparams = {'n_estimators': 40, 'max_depth': None}
 svm_hyperparams = {'C': 0.1, 'kernel': 'rbf'}
 mlp_hyperparams = {'hidden_layer_sizes': (100, 100, 100, 100, 100), 'alpha': 100, 'max_iter': 1000, 'solver': 'adam',
                    'activation': 'identity', 'tol': 1.}
+
 hyperparameters = {'linear_regressor': ridge_hyperparams,
                    'dt_regressor': dt_hyperparams, 'dt_classifier': dt_hyperparams,
                    'rf_regressor': rf_hyperparams, 'rf_classifier': rf_hyperparams,
                    'svm_regressor': svm_hyperparams, 'svm_classifier': svm_hyperparams,
                    'mlp_regressor': mlp_hyperparams, 'mlp_classifier': mlp_hyperparams}
+
 
 # Sklearn models
 skl_models = {}
@@ -52,6 +57,7 @@ skl_models['svm_classifier'] = svm.SVC(**svm_hyperparams)
 skl_models['mlp_regressor'] = neural_network.MLPRegressor(**mlp_hyperparams)
 skl_models['mlp_classifier'] = neural_network.MLPClassifier(**mlp_hyperparams)
 
+
 # Fit sklearn models
 for key, model in skl_models.items():
     if 'regressor' in key:
@@ -62,17 +68,20 @@ for key, model in skl_models.items():
         continue
     model.fit(std_inputs, labelled_outputs)
 
+
 # Dessia models
 dessia_classes = {'linear_regressor': LinearRegression, 'dt_regressor': DecisionTreeRegressor,
                  'dt_classifier': DecisionTreeClassifier, 'rf_regressor': RandomForestRegressor,
                  'rf_classifier': RandomForestClassifier, 'svm_regressor': SVR, 'svm_classifier': SVC,
                  'mlp_regressor': MLPRegressor, 'mlp_classifier': MLPClassifier}
 
+
 # Assert regenerated sklearn models from dessia models make the same predictions as sklearn models from sklearn.fit
 dessia_models = {}
 for key, model in skl_models.items():
     dessia_models[key] = dessia_classes[key]._instantiate_dessia(model)
     assert(npy.all(dessia_models[key].predict(std_inputs[50:100]) == model.predict(std_inputs[50:100])))
+
 
 # Test dessia models methods
 dessia_models = {}
@@ -90,6 +99,7 @@ for key, model in skl_models.items():
     dessia_models[key] = dessia_classes[key].fit(std_inputs, local_outputs, **params)
     assert(isinstance(dessia_models[key].score(std_inputs, local_outputs), float))
     dessia_models[key]._check_platform()
+
 
 # Tests errors and base objects
 base_scaler = BaseScaler()
