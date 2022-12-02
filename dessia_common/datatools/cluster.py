474676aeb33603a458c65ec2566bf26644a84017
Tanguy-DES <tanguy.loreau@dessia.tech>
2022-12-02 15:18:37 +0100

    feat(machine_learning): new test for blank line period

--- dessia_common/datatools/cluster.py
+++ dessia_common/datatools/cluster.py
@@ -426,8 +426,8 @@ class ClusteredDataset(Dataset):
     @classmethod
     def from_kmeans(cls, data: Dataset, n_clusters: int = 2, n_init: int = 10, tol: float = 1e-4,
                     scaling: bool = False, name: str = ""):
-        """
-        The KMeans algorithm clusters data by trying to separate samples in n groups of equal variance,
+        """The KMeans algorithm clusters data by trying to separate samples in n groups of equal variance.
+
         minimizing a criterion known as the inertia or within-cluster sum-of-squares (see below).
         This algorithm requires the number of clusters to be specified. It scales well to large number
         of samples and has been used across a large range of application areas in many different fields.
