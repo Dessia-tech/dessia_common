1e83dc1e177d0d9276b8cd13e9936ff3ba051f89
Steven Masfaraud <masfaraud@dessia.tech>
2023-01-02 17:27:51 +0100

    fix: ci

--- code_changelog.sh
+++ code_changelog.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 lines=$(git diff $DRONE_TARGET_BRANCH..$DRONE_SOURCE_BRANCH -- CHANGELOG.md --unified=0| wc -l)
 echo "$lines lines modified on CHANGELOG.md in PR $DRONE_SOURCE_BRANCH -> $DRONE_TARGET_BRANCH"
 
