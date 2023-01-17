99356daa89ce8cbd5e8267893bf97444a67803e5
Steven Masfaraud <masfaraud@dessia.tech>
2023-01-17 14:58:56 +0100

    fix: pylint note

--- code_pylint.py
+++ code_pylint.py
@@ -18,7 +18,7 @@ from datetime import date
 from pylint import __version__
 from pylint.lint import Run
 
-MIN_NOTE = 9.6
+MIN_NOTE = 9.4
 
 EFFECTIVE_DATE = date(2023, 1, 18)
 WEEKLY_DECREASE = 0.03
