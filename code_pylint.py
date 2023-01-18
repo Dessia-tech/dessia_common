241ac7ff3acc793e0a8681e77dea2b397354ce84
jezequel <jezequel@dessia.tech>
2023-01-18 17:16:53 +0100

    fix: reset thresholds and add comments

--- code_pylint.py
+++ code_pylint.py
@@ -26,29 +26,22 @@ WEEKLY_DECREASE = 0.03
 UNWATCHED_ERRORS = ["fixme", "trailing-whitespace", "import-error"]
 
 MAX_ERROR_BY_TYPE = {
-    # "protected-access": 38,
-    # "arguments-differ": 2,
-    # "no-member": 3,
-    # "too-many-locals": 10,  # Reduce by dropping vectored objects
-    # "too-many-branches": 13,
-    # "unused-argument": 7,
-    # "cyclic-import": 4,
-    # "missing-module-docstring": 1,
-    # "too-many-arguments": 21,
-    # "too-few-public-methods": 9,
-    # "too-many-return-statements": 10,
-    # "import-outside-toplevel": 4,  # TODO : will reduced in a future work (when tests are ready)
-    # "too-many-instance-attributes": 7,
-    # "attribute-defined-outside-init": 3,
-    # "broad-except": 4,
-    # "bare-except": 4,
-    # "too-many-nested-blocks": 2,
-    # "too-many-public-methods": 2,  # Try to lower by splitting DessiaObject and Workflow
-    # "too-many-statements": 1,
-    # "duplicate-code": 1,
-    # "too-many-lines": 1,
-    # "abstract-method": 6,
-    # "undefined-loop-variable": 1,
+    "protected-access": 38,  # Highly dependant on our "private" conventions. Keeps getting raised
+    "arguments-differ": 1,
+    "too-many-locals": 10,  # Reduce by dropping vectored objects
+    "too-many-branches": 13,  # Huge refactor needed. Will be reduced by schema refactor
+    "unused-argument": 5,  # Some abstract functions have unused arguments (plot_data). Hence cannot decrease
+    "cyclic-import": 2,  # Still work to do on Specific based DessiaObject
+    "too-many-arguments": 21,  # Huge refactor needed
+    "too-few-public-methods": 9,  # Abstract classes (Errors, Checks,...)
+    "too-many-return-statements": 10, #  Huge refactor needed. Will be reduced by schema refactor
+    "import-outside-toplevel": 4,  # TODO : will reduced in a future work (when tests are ready)
+    "too-many-instance-attributes": 7,  # Huge refactor needed (workflow, etc...)
+    "broad-except": 4,  # Necessary in order not to raise non critical errors. Will be reduced by schema refactor
+    "bare-except": 3,  # Necessary in order not to raise non critical errors. Will be reduced by schema refactor
+    "too-many-public-methods": 2,  # Try to lower by splitting DessiaObject and Workflow
+    "too-many-statements": 1,  # Will be solved by schema refactor and jsonchema removal
+    "undefined-loop-variable": 1,  # Fearing to break the code by solving it
 }
 
 ERRORS_WITHOUT_TIME_DECREASE = ["bare-except"]
