1a70a20a73393737e375f1ae7ed8097c6be5395a
Thiefaine-PC <thiefaine.breton@gmail.com>
2023-04-03 16:50:00 +0200

    refactor(tests): use the expand parameterized to lunch multiple tests

--- tests/test_workflow/test_methode_attribute.py
+++ tests/test_workflow/test_methode_attribute.py
@@ -1,15 +1,12 @@
-from dessia_common import tests
 import unittest
-from dessia_common import core
 from dessia_common.workflow.blocks import GetModelAttribute, SetModelAttribute
 from dessia_common.workflow.core import Variable, TypedVariable
-import inspect
 from dessia_common.typings import AttributeType
 from dessia_common.forms import DessiaObject
-import time
 from typing import List, Dict
+from parameterized import parameterized
 
-class UnrealClassForTesting(DessiaObject):
+class DummyClassForTesting(DessiaObject):
     def __init__(self, int_: int , variable_, float_: float = 0.0, str_: str = 'test', bool_: bool = True,
                   l: List[int] = [1,2,3], d: Dict[str,int] = {'a':1,'b':2}, two: int = 2, name: str ='', **kwargs):
         self.int_ = int_
@@ -33,116 +30,95 @@ class TestGetModelAttribute(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_outputs(self):
-        instanciate = [(GetModelAttribute(AttributeType(UnrealClassForTesting,'int_')), 
-                        TypedVariable(type_=int, name='Model attribute')),
-                        (GetModelAttribute(AttributeType(UnrealClassForTesting,'float_')), 
-                         TypedVariable(type_=float, name='Model attribute')),
-                        (GetModelAttribute(AttributeType(UnrealClassForTesting,'str_')), 
-                         TypedVariable(type_=str, name='Model attribute')),
-                        (GetModelAttribute(AttributeType(UnrealClassForTesting,'bool_')), 
-                         TypedVariable(type_=bool, name='Model attribute')),
-                        (GetModelAttribute(AttributeType(UnrealClassForTesting,'l')), 
-                         TypedVariable(type_=List[int], name='Model attribute')),
-                        (GetModelAttribute(AttributeType(UnrealClassForTesting,'d')), 
-                         TypedVariable(type_=Dict[str,int], name='Model attribute')),
-                        (GetModelAttribute(AttributeType(UnrealClassForTesting,'two')), 
-                         TypedVariable(type_=int, name='Model attribute')),
-                        (GetModelAttribute(AttributeType(UnrealClassForTesting,'name')), 
-                         TypedVariable(type_=str, name='Model attribute')),
-                        (GetModelAttribute(AttributeType(UnrealClassForTesting,'combine')), 
-                         Variable(name='Model attribute')),
-                        (GetModelAttribute(AttributeType(UnrealClassForTesting,'variable_')), 
-                         Variable(name='Model attribute')),
-                        (GetModelAttribute(AttributeType(UnrealClassForTesting,'kwargs')), 
-                         Variable(name='Model attribute'))]
-        for el in instanciate:
-            if type(el[1])==Variable:
-                self.assertTrue(el[0].outputs[0]._data_eq(el[1]))
-            else:
-                self.assertTrue(el[0].outputs[0].name == el[1].name)
-                self.assertTrue(el[0].outputs[0].type_ == el[1].type_)
-        pass
+    @parameterized.expand([
+        (int, 'int_'),
+        (float, 'float_'),
+        (str, 'str_'),
+        (bool, 'bool_'),
+        (List[int], 'l'),
+        (Dict[str,int], 'd'),
+        (int, 'two'),
+        (str, 'name'),
+        (None, 'variable_'),
+        (None, 'combine'),
+        (None, 'kwargs')])
+    def test_outputs(self, type_, name):
+        GMA = GetModelAttribute(AttributeType(DummyClassForTesting,name))
+        if type_:
+            TV = TypedVariable(type_=type_, name='Model attribute')
+            self.assertTrue(GMA.outputs[0].name == TV.name)
+            self.assertTrue(GMA.outputs[0].type_ == TV.type_)
+        else:
+            V = Variable(name='Model attribute')
+            self.assertTrue(GMA.outputs[0]._data_eq(V))
 
-    def test_getmethod_equivalent(self):
-        instanciate = [GetModelAttribute(AttributeType(UnrealClassForTesting,'int_')),
-                        GetModelAttribute(AttributeType(UnrealClassForTesting,'float_')),
-                        GetModelAttribute(AttributeType(UnrealClassForTesting,'str_')),
-                        GetModelAttribute(AttributeType(UnrealClassForTesting,'bool_')),
-                        GetModelAttribute(AttributeType(UnrealClassForTesting,'l')),
-                        GetModelAttribute(AttributeType(UnrealClassForTesting,'d')),
-                        GetModelAttribute(AttributeType(UnrealClassForTesting,'two')),
-                        GetModelAttribute(AttributeType(UnrealClassForTesting,'name')),
-                        GetModelAttribute(AttributeType(UnrealClassForTesting,'tuple_')),
-                        GetModelAttribute(AttributeType(UnrealClassForTesting,'list_of_tuple')),
-                        GetModelAttribute(AttributeType(UnrealClassForTesting,'combine')),
-                        GetModelAttribute(AttributeType(UnrealClassForTesting,'variable_')),
-                        GetModelAttribute(AttributeType(UnrealClassForTesting,'kwargs'))]
-        for el in instanciate:
-            self.assertTrue(el.equivalent(el.__deepcopy__()))
+
+    @parameterized.expand([
+        'int_',
+        'float_',
+        'str_',
+        'bool_',
+        'l',
+        'd',
+        'two',
+        'name',
+        'variable_',
+        'combine',
+        'kwargs',
+        'tuple_',
+        'list_of_tuple'])
+    def test_getmethod_equivalent(self, name):
+        GMA = GetModelAttribute(AttributeType(DummyClassForTesting,name))
+        self.assertTrue(GMA.equivalent(GMA.__deepcopy__()))
 
 
 class TestSetModelAttribute(unittest.TestCase):          
     def setUp(self):
         pass
 
-    def test_outputs(self):
-        instanciate = [(SetModelAttribute(AttributeType(UnrealClassForTesting,'int_')),
-                        TypedVariable(type_=UnrealClassForTesting, name='Model with changed attribute int_'), 
-                        TypedVariable(type_=int, name='Value to insert for attribute int_')),
-                        (SetModelAttribute(AttributeType(UnrealClassForTesting,'float_')), 
-                         TypedVariable(type_=UnrealClassForTesting, name='Model with changed attribute float_'), 
-                         TypedVariable(type_=float, name='Value to insert for attribute float_')),
-                        (SetModelAttribute(AttributeType(UnrealClassForTesting,'str_')), 
-                         TypedVariable(type_=UnrealClassForTesting, name='Model with changed attribute str_'), 
-                         TypedVariable(type_=str, name='Value to insert for attribute str_')),
-                        (SetModelAttribute(AttributeType(UnrealClassForTesting,'bool_')), 
-                         TypedVariable(type_=UnrealClassForTesting, name='Model with changed attribute bool_'), 
-                         TypedVariable(type_=bool, name='Value to insert for attribute bool_')),
-                        (SetModelAttribute(AttributeType(UnrealClassForTesting,'l')), 
-                         TypedVariable(type_=UnrealClassForTesting, name='Model with changed attribute l'), 
-                         TypedVariable(type_=List[int], name='Value to insert for attribute l')),
-                        (SetModelAttribute(AttributeType(UnrealClassForTesting,'d')), 
-                         TypedVariable(type_=UnrealClassForTesting, name='Model with changed attribute d'), 
-                         TypedVariable(type_=Dict[str,int], name='Value to insert for attribute d')),
-                        (SetModelAttribute(AttributeType(UnrealClassForTesting,'two')), 
-                         TypedVariable(type_=UnrealClassForTesting, name='Model with changed attribute two'), 
-                         TypedVariable(type_=int, name='Value to insert for attribute two')),
-                        (SetModelAttribute(AttributeType(UnrealClassForTesting,'combine')), 
-                         TypedVariable(type_=UnrealClassForTesting, name='Model with changed attribute combine'), 
-                         Variable(name='Value to insert for attribute combine')),
-                        (SetModelAttribute(AttributeType(UnrealClassForTesting,'variable_')), 
-                         TypedVariable(type_=UnrealClassForTesting, name='Model with changed attribute variable_'), 
-                         Variable(name='Value to insert for attribute variable_')),
-                        (SetModelAttribute(AttributeType(UnrealClassForTesting,'kwargs')), 
-                         TypedVariable(type_=UnrealClassForTesting, name='Model with changed attribute kwargs'), 
-                         Variable(name='Value to insert for attribute kwargs'))]
-        for el in instanciate:
-            self.assertTrue(el[0].outputs[0].name == el[1].name)
-            self.assertTrue(el[0].outputs[0].type_ == el[1].type_)
-            if type(el[2])==Variable:
-                self.assertTrue(el[0].inputs[1]._data_eq(el[2]))
-            else:
-                self.assertTrue(el[0].inputs[1].name == el[2].name)
-                self.assertTrue(el[0].inputs[1].type_ == el[2].type_)
+    @parameterized.expand([
+        (int, 'int_'),
+        (float, 'float_'),
+        (str, 'str_'),
+        (bool, 'bool_'),
+        (List[int], 'l'),
+        (Dict[str,int], 'd'),
+        (int, 'two'),
+        (str, 'name'),
+        (None, 'variable_'),
+        (None, 'combine'),
+        (None, 'kwargs')])
+    def test_outputs(self, type_, name):
+        SMA = SetModelAttribute(AttributeType(DummyClassForTesting, name))
+        TV_output = TypedVariable(type_=DummyClassForTesting, name='Model with changed attribute '+name)
+        self.assertTrue(SMA.outputs[0].name == TV_output.name)
+        self.assertTrue(SMA.outputs[0].type_ == TV_output.type_)
+        if type_:
+            TV_input = TypedVariable(type_=type_, name='Value to insert for attribute '+name)
+            self.assertTrue(SMA.inputs[1].name == TV_input.name)
+            self.assertTrue(SMA.inputs[1].type_ == TV_input.type_)
+        else:
+            V_input = Variable(name='Value to insert for attribute '+name)
+            self.assertTrue(SMA.inputs[1]._data_eq(V_input))
         pass
 
-    def test_setmethod_equivalent(self):
-        instanciate = [SetModelAttribute(AttributeType(UnrealClassForTesting,'int_')),
-                        SetModelAttribute(AttributeType(UnrealClassForTesting,'float_')),
-                        SetModelAttribute(AttributeType(UnrealClassForTesting,'str_')),
-                        SetModelAttribute(AttributeType(UnrealClassForTesting,'bool_')),
-                        SetModelAttribute(AttributeType(UnrealClassForTesting,'list_')),
-                        SetModelAttribute(AttributeType(UnrealClassForTesting,'d')),
-                        SetModelAttribute(AttributeType(UnrealClassForTesting,'two')),
-                        SetModelAttribute(AttributeType(UnrealClassForTesting,'name')),
-                        SetModelAttribute(AttributeType(UnrealClassForTesting,'tuple_')),
-                        SetModelAttribute(AttributeType(UnrealClassForTesting,'list_of_tuple')),
-                        SetModelAttribute(AttributeType(UnrealClassForTesting,'combine')),
-                        SetModelAttribute(AttributeType(UnrealClassForTesting,'variable_')),
-                        SetModelAttribute(AttributeType(UnrealClassForTesting,'kwargs'))]
-        for el in instanciate:
-            self.assertTrue(el.equivalent(el.__deepcopy__()))
+    @parameterized.expand([
+        'int_',
+        'float_',
+        'str_',
+        'bool_',
+        'l',
+        'd',
+        'two',
+        'name',
+        'variable_',
+        'combine',
+        'kwargs',
+        'tuple_',
+        'list_of_tuple'])
+    def test_setmethod_equivalent(self, name):
+        SMA = SetModelAttribute(AttributeType(DummyClassForTesting,name))
+        self.assertTrue(SMA.equivalent(SMA.__deepcopy__()))
 
 if __name__ == '__main__':
     unittest.main()
