a35e7a2843ba02a37b2b6d92937d865da622ed6a
younesdessia <younes.belabid@dessia.io>
2023-04-17 22:33:21 +0200

    add error to errors without time effect

--- code_pylint.py
+++ code_pylint.py
@@ -54,7 +54,7 @@ ERRORS_WITHOUT_TIME_DECREASE = ['protected-access', 'arguments-differ', 'too-man
                                 'too-many-return-statements', 'import-outside-toplevel',
                                 'too-many-instance-attributes', 'broad-except', 'bare-except', "broad-exception-caught",
                                 'too-many-public-methods', 'too-many-statements', 'undefined-loop-variable',
-                                'attribute-defined-outside-init']
+                                'attribute-defined-outside-init', 'wrong-spelling-in-docstring']
 
 print("pylint version: ", __version__)
 
