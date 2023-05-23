58aa2b5cee03a6d90d91df1a8786c97d3211cf3d
Ghislain Jézéquel <jezequel@dessia.tech>
2023-05-23 17:09:19 +0200

    tests(schemas): refactor tests to use unit tests

--- tests/test_schemas/test_computation_sequences.py
+++ tests/test_schemas/test_computation_sequences.py
@@ -5,27 +5,21 @@ from typing import List, Tuple
 import unittest
 from parameterized import parameterized
 
-# computed_schema = schema.to_dict(title="Bare Tuple", editable=True, description="Testing Tuple with no arg")
-# assert computed_schema == {'additionalItems': False,
-#                            'type': 'array',
-#                            'items': [],
-#                            'title': 'Bare Tuple',
-#                            'editable': True,
-#                            'description': 'Testing Tuple with no arg',
-#                            'python_typing': 'Tuple'}
-
 
 class TestFaulty(unittest.TestCase):
     @parameterized.expand([
         (HomogeneousSequence(annotation=List, attribute="bare_list"), 2),
         (HeterogeneousSequence(annotation=Tuple, attribute="bare_tuple"), 2)
-
     ])
     def test_schema_check(self, schema, expected_number):
+        computed_schema = schema.to_dict()
+        self.assertEqual(computed_schema["type"], "array")
+        self.assertEqual(computed_schema["items"], [])
+
         checked_schema = schema.check_list()
-        assert checked_schema.checks_above_level("error")
-        assert len(checked_schema) == expected_number
-        assert checked_schema[0].level == "error"
+        self.assertTrue(checked_schema.checks_above_level("error"))
+        self.assertEqual(len(checked_schema), expected_number)
+        self.assertEqual(checked_schema[0].level, "error")
 
 
 class TestSequences(unittest.TestCase):
@@ -68,3 +62,26 @@ class TestSequences(unittest.TestCase):
 
         checked_schema = schema.check_list()
         self.assertFalse(checked_schema.checks_above_level("error"))
+
+    @parameterized.expand([
+        (HeterogeneousSequence(annotation=Tuple[int, str], attribute="two_element_tuple"), "Tuple[int, str]"),
+    ])
+    def test_simple_sequences(self, schema, expected_typing):
+        computed_schema = schema.to_dict("Sequence")
+        self.assertEqual(computed_schema["type"], "array")
+        self.assertEqual(computed_schema["additionalItems"], False)
+        self.assertEqual(computed_schema["title"], "Sequence")
+        self.assertEqual(computed_schema["python_typing"], expected_typing)
+        self.assertEqual(computed_schema["items"][0]["type"], "number")
+        self.assertEqual(computed_schema["items"][0]["python_typing"], "int")
+        self.assertEqual(computed_schema["items"][0]["title"], "Sequence/0")
+        self.assertEqual(computed_schema["items"][0]["type"], "string")
+        self.assertEqual(computed_schema["items"][0]["python_typing"], "str")
+        self.assertEqual(computed_schema["items"][0]["title"], "Sequence/1")
+
+        checked_schema = schema.check_list()
+        self.assertFalse(checked_schema.checks_above_level("error"))
+
+        # TODO Should object be allowed here ?
+        # more_arg_tuple = schema_chunk(annotation=Tuple[int, str, StandaloneObject], title="More Arg Tuple",
+        #                               editable=True, description="Testing Tuple with several args")
