349bf6f77c54b37f9d5dbe1c25b61de4e3aae272
Ghislain Jézéquel <jezequel@dessia.tech>
2023-09-01 16:17:18 +0200

    tests: fix variable matching

--- tests/test_workflow/test_forms_simulation.py
+++ tests/test_workflow/test_forms_simulation.py
@@ -9,7 +9,7 @@ n_solutions = 100
 input_values = {workflow_.input_index(parameter_input): 5,
                 workflow_.input_index(integer_input): n_solutions,
                 workflow_.input_index(string_input): "Test"}
-workflow_run = workflow_.run(input_values=input_values, verbose=True, name='Dev Objects')
+workflow_run = workflow_.run(input_values=input_values, verbose=True, name="Dev Objects")
 
 
 class TestWorkflowFeatures(unittest.TestCase):
@@ -26,20 +26,20 @@ class TestWorkflowFeatures(unittest.TestCase):
         self.assertEqual(hash(object_), hash(deserialized_object))
 
     @parameterized.expand([
-        ('(0, 0, 0)', ['0']),
-        ('(0, 0, 1)', ['0']),
-        ('(0, 0, 2)', ['(1, 1, 0)']),
-        ('(0, 0, 3)', ['1']),
-        ('(0, 1, 0)', ['(1, 0, 0)']),
-        ('(1, 0, 0)', ['(0, 1, 0)']),
-        ('(1, 1, 0)', ['(0, 0, 2)', '(4, 0, 0)']),
-        ('(1, 1, 1)', []),
-        ('(3, 0, 1)', ['1']),
-        ('(3, 0, 2)', ['0']),
-        ('(4, 0, 0)', []),
-        ('(4, 1, 0)', []),
-        ('0', ['(0, 0, 0)', '(0, 0, 1)', '(3, 0, 2)']),
-        ('1', ['(0, 0, 3)', '(3, 0, 1)']),
+        ("(0, 0, 0)", ["0"]),
+        ("(0, 0, 1)", ["0"]),
+        ("(0, 0, 2)", ["(1, 1, 0)", "(2, 1, 0)"]),
+        ("(0, 0, 3)", ["1"]),
+        ("(0, 1, 0)", ["(1, 0, 0)", "(2, 0, 0)"]),
+        ("(1, 0, 0)", ["(0, 1, 0)"]),
+        ("(1, 1, 0)", ["(0, 0, 2)", "(4, 0, 0)"]),
+        ("(1, 1, 1)", ["(2, 0, 0)"]),
+        ("(3, 0, 1)", ["1"]),
+        ("(3, 0, 2)", ["0"]),
+        ("(4, 0, 0)", []),
+        ("(4, 1, 0)", []),
+        ("0", ["(0, 0, 0)", "(0, 0, 1)", "(3, 0, 2)"]),
+        ("1", ["(0, 0, 3)", "(3, 0, 1)"]),
     ])
     def test_variables_match(self, variable, expected_match):
         variables_match = workflow_.match_variables(True)
@@ -62,4 +62,4 @@ class TestWorkflowFeatures(unittest.TestCase):
 
     def test_arguments(self):
         arguments = workflow_.dict_to_arguments(input_values, "run")
-        self.assertDictEqual(arguments, {'input_values': {0: 5, 1: None, 2: 3, 3: 100, 4: 'Test'}, 'name': None})
+        self.assertDictEqual(arguments, {"input_values": {0: 5, 1: None, 2: 3, 3: 100, 4: "Test"}, "name": None})
