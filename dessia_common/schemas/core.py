577ece0a4a632406e880af2b37b81884af0fdeb9
Ghislain Jézéquel <jezequel@dessia.tech>
2023-12-07 14:59:55 +0100

    docs(docstrings): add for newly created methods

--- dessia_common/schemas/core.py
+++ dessia_common/schemas/core.py
@@ -439,6 +439,7 @@ class Property:
 
     @cached_property
     def is_file_related(self) -> bool:
+        """ Wether the property is related to files. Base property never is. """
         return False
 
     @cached_property
@@ -530,6 +531,7 @@ class TypingProperty(Property):
 
     @cached_property
     def is_file_related(self) -> bool:
+        """ Wether the property is related to files. Is related to files if any of its argument is. """
         return any(s.is_file_related for s in self.args_schemas)
 
     @cached_property
@@ -774,6 +776,7 @@ class FileProperty(Property):
 
     @cached_property
     def is_file_related(self) -> bool:
+        """ Wether the property is related to files. Base property always is. """
         return True
 
     @cached_property
