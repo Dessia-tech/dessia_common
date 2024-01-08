9e1a52da25e6eab95eede87f860f17128faae7ff
Ghislain Jézéquel <jezequel@dessia.tech>
2024-01-08 16:06:35 +0100

    test(workflow): close string file while teardown

--- tests/test_workflow/test_file_inputs.py
+++ tests/test_workflow/test_file_inputs.py
@@ -8,14 +8,17 @@ from dessia_common.files import StringFile
 class WorkflowInputTests(unittest.TestCase):
     def setUp(self) -> None:
         ref = importlib.resources.files("dessia_common").joinpath("models/data/seed_file.csv")
-        with ref.open("rb") as csv_cars:
-            self.stringfile = StringFile.from_stream(csv_cars)
+        with ref.open("rb") as stream:
+            self.stringfile = StringFile.from_stream(stream)
 
     def test_run_by_string_file_input(self):
         inputs = {0: self.stringfile}
         workflow_run = workflow_.run(input_values=inputs)
         self.assertEqual(workflow_run.output_value.standalone_subobject.intarg, 7)
         workflow_run._check_platform()
+
+    def tearDown(self) -> None:
+        self.stringfile.close()
 
 
 if __name__ == '__main__':
