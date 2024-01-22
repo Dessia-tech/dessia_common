cc16a88452a572fed235bec1c62c26a8dab20daf
younesdessia <younes.belabid@dessia.io>
2024-01-22 10:20:34 +0100

    Perf unit test

--- tests/test_workflow/test_to_script.py
+++ tests/test_workflow/test_to_script.py
@@ -1,7 +1,11 @@
 import unittest
-from typing import List, Dict, Tuple
+from typing import List, Dict, Tuple, Union
 
-from dessia_common.typings import MethodType, MarkdownType, PlotDataType
+from dessia_common.forms import EmbeddedSubobject, StandaloneBuiltinsSubobject
+from dessia_common.measures import Measure, Distance
+from parameterized import parameterized
+
+from dessia_common.typings import MethodType, MarkdownType, PlotDataType, InstanceOf
 from dessia_common.workflow.core import Pipe, Workflow, Variable
 from dessia_common.workflow.blocks import InstantiateModel, ModelMethod, PlotData, Markdown, Export
 from dessia_common.decorators import markdown_view, plot_data_view
@@ -17,18 +21,28 @@ class ConnectionNode(DessiaObject):
 
 
 class ExperimentObject(DessiaObject):
-    def __init__(self, name: str = "", obj_inter: List[ConnectionNode] = None,
-                 file: Tuple[BinaryFile, BinaryFile] = None,
-                 numbers: List[int] = None, bools: List[float] = None, strings: List[str] = None,
-                 dict_val: Dict[str, int] = None,
-                 list_tuple: List[List[Tuple[float, float]]] = None):
-        self.obj_inter = obj_inter
-        self.file = file
-        self.numbers = numbers
-        self.bools = bools
+    def __init__(self, name: str = "", connections: List[ConnectionNode] = None,
+                 binary_files: Tuple[BinaryFile, BinaryFile] = None,
+                 integers: List[int] = None, floats: List[float] = None, strings: List[str] = None,
+                 dictionary: Dict[str, int] = None,
+                 nested_tuples: List[List[Tuple[float, float]]] = None,
+                 embedded_object: InstanceOf[EmbeddedSubobject] = None,
+                 union_object: Union[EmbeddedSubobject, StandaloneBuiltinsSubobject] = None,
+                 nested_lists: List[List[List[Tuple[float, float]]]] = None,
+                 measure_object: Measure = None,
+                 distance_object: Distance = None):
+        self.connections = connections
+        self.binary_files = binary_files
+        self.integers = integers
+        self.floats = floats
         self.strings = strings
-        self.dict_val = dict_val
-        self.list_tuple = list_tuple
+        self.dictionary = dictionary
+        self.nested_tuples = nested_tuples
+        self.embedded_object = embedded_object
+        self.union_object = union_object
+        self.nested_lists = nested_lists
+        self.measure_object = measure_object
+        self.distance_object = distance_object
         super().__init__(name=name)
 
     def run_driver(self):
@@ -71,22 +85,61 @@ block_5 = [Export(method_type=MethodType(ExperimentObject, 'save_to_stream'), na
 blocks = [block_0, block_1, block_2, block_4, block_5]
 
 # Variables
-variable_list = [[Variable(name='variable_0', position=[-6.77, 34.65], type_=str),
-                  "Variable(name='variable_0', position=[-6.77, 34.65], type_=str)"],
-                 [Variable(name='variable_1', position=[-632.77, 34.65], type_=List[BinaryFile]),
-                  "Variable(name='variable_1', position=[-632.77, 34.65], type_=List[BinaryFile])"],
-                 [Variable(name='variable_2', position=[-2342.61, 18.72], type_=List[ConnectionNode]),
-                  "Variable(name='variable_2', position=[-2342.61, 18.72], type_=List[ConnectionNode])"],
-                 [Variable(name='variable_3', position=[197.91, 95.02], type_=List[int]),
-                  "Variable(name='variable_3', position=[197.91, 95.02], type_=List[int])"],
-                 [Variable(name='variable_4', position=[-1035.71, -269.35], type_=List[str]),
-                  "Variable(name='variable_4', position=[-1035.71, -269.35], type_=List[str])"],
-                 [Variable(name='variable_5', position=[2620.96, -486.85], type_=List[bool]),
-                  "Variable(name='variable_5', position=[2620.96, -486.85], type_=List[bool])"],
-                 [Variable(name='variable_6', position=[2620.96, -486.85], type_=Dict[str, int]),
-                  "Variable(name='variable_6', position=[2620.96, -486.85], type_=Dict[str, int])"],
-                 [Variable(name='variable_7', position=[2620.9, -486.8], type_=List[List[Tuple[float, float]]]),
-                  "Variable(name='variable_7', position=[2620.9, -486.8], type_=List[List[Tuple[float, float]]])"]]
+variable_list = [
+    [
+        Variable(name='variable_0', position=[-6.77, 34.65], type_=str),
+        "Variable(name='variable_0', position=[-6.77, 34.65], type_=str)"
+    ],
+    [
+        Variable(name='variable_1', position=[-632.77, 34.65], type_=List[BinaryFile]),
+        "Variable(name='variable_1', position=[-632.77, 34.65], type_=List[BinaryFile])"
+    ],
+    [
+        Variable(name='variable_2', position=[-2342.61, 18.72], type_=List[ConnectionNode]),
+        "Variable(name='variable_2', position=[-2342.61, 18.72], type_=List[ConnectionNode])"
+    ],
+    [
+        Variable(name='variable_3', position=[197.91, 95.02], type_=List[int]),
+        "Variable(name='variable_3', position=[197.91, 95.02], type_=List[int])"
+    ],
+    [
+        Variable(name='variable_4', position=[-1035.71, -269.35], type_=List[str]),
+        "Variable(name='variable_4', position=[-1035.71, -269.35], type_=List[str])"
+    ],
+    [
+        Variable(name='variable_5', position=[2620.96, -486.85], type_=List[bool]),
+        "Variable(name='variable_5', position=[2620.96, -486.85], type_=List[bool])"
+    ],
+    [
+        Variable(name='variable_6', position=[2620.96, -486.85], type_=Dict[str, int]),
+        "Variable(name='variable_6', position=[2620.96, -486.85], type_=Dict[str, int])"
+    ],
+    [
+        Variable(name='variable_7', position=[2620.9, -486.8], type_=List[List[Tuple[float, float]]]),
+        "Variable(name='variable_7', position=[2620.9, -486.8], type_=List[List[Tuple[float, float]]])"
+    ],
+    [
+        Variable(name='variable_8', position=[2620.96, -486.84], type_=InstanceOf[EmbeddedSubobject]),
+        "Variable(name='variable_8', position=[2620.96, -486.84], type_=InstanceOf[EmbeddedSubobject])"
+    ],
+    [
+        Variable(name='variable_9', position=[2.6, -4.8], type_=Union[EmbeddedSubobject, StandaloneBuiltinsSubobject]),
+        "Variable(name='variable_9', position=[2.6, -4.8], type_=Union[EmbeddedSubobject, StandaloneBuiltinsSubobject])"
+    ],
+    [
+        Variable(name='variable_10', position=[2620.96, -486.84], type_=List[List[List[Tuple[float, float]]]]),
+        "Variable(name='variable_10', position=[2620.96, -486.84], type_=List[List[List[Tuple[float, float]]]])"
+    ],
+    [
+        Variable(name='variable_11', position=[26.96, -486.84], type_=Measure),
+        "Variable(name='variable_11', position=[26.96, -486.84], type_=Measure)"
+    ],
+    [
+        Variable(name='variable_12', position=[26.96, -486.84], type_=Distance),
+        "Variable(name='variable_12', position=[26.96, -486.84], type_=Distance)"
+    ]
+]
+
 
 # Pipes
 pipe_1 = Pipe(blocks[0][0].outputs[0], blocks[1][0].inputs[0])
@@ -98,26 +151,34 @@ pipe_6 = Pipe(variable_list[5][0], blocks[0][0].inputs[4])
 pipe_7 = Pipe(variable_list[0][0], blocks[0][0].inputs[0])
 pipe_8 = Pipe(variable_list[6][0], blocks[0][0].inputs[6])
 pipe_9 = Pipe(variable_list[7][0], blocks[0][0].inputs[7])
-pipe_10 = Pipe(blocks[1][0].outputs[0], blocks[2][0].inputs[0])
-pipe_11 = Pipe(blocks[1][0].outputs[0], blocks[3][0].inputs[0])
+
+pipe_10 = Pipe(variable_list[8][0], blocks[0][0].inputs[8])
+pipe_11 = Pipe(variable_list[9][0], blocks[0][0].inputs[9])
+pipe_12 = Pipe(variable_list[10][0], blocks[0][0].inputs[10])
+pipe_13 = Pipe(variable_list[11][0], blocks[0][0].inputs[11])
+pipe_14 = Pipe(variable_list[12][0], blocks[0][0].inputs[12])
+
+pipe_15 = Pipe(blocks[1][0].outputs[0], blocks[2][0].inputs[0])
+pipe_16 = Pipe(blocks[1][0].outputs[0], blocks[3][0].inputs[0])
+
 
 workflow = Workflow([block[0] for block in blocks],
                     [
                         pipe_1, pipe_2, pipe_3, pipe_4, pipe_5, pipe_6, pipe_7,
-                        pipe_8, pipe_9, pipe_10, pipe_11
+                        pipe_8, pipe_9, pipe_10, pipe_11, pipe_12, pipe_13, pipe_14, pipe_15, pipe_16
                     ],
                     output=blocks[1][0].outputs[0], name="TestExport")
 
 
 class WorkflowToScriptTest(unittest.TestCase):
 
-    def test_blocks(self):
-        for block in blocks:
-            self.assertEqual(block[0]._to_script("").declaration, block[1])
+    @parameterized.expand(blocks)
+    def test_blocks(self, block, expected_declaration):
+        self.assertEqual(block._to_script("").declaration, expected_declaration)
 
-    def test_variables(self):
-        for variable in variable_list:
-            self.assertEqual(variable[0]._to_script().declaration, variable[1])
+    @parameterized.expand(variable_list)
+    def test_variables(self, variable, expected_declaration):
+        self.assertEqual(variable._to_script().declaration, expected_declaration)
 
     def test_workflow(self):
         workflow_script = workflow.to_script()
