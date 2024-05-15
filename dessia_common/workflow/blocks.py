693b36643e0f07828ab6f2152fdbc33c23f135c1
GhislainJ <ghislain.jezequel@gmail.com>
2024-05-15 09:37:40 +0200

    feat(blocks): add multi-object block

--- dessia_common/workflow/blocks.py
+++ dessia_common/workflow/blocks.py
@@ -934,6 +934,94 @@ class MultiPlot(Display):
         return block
 
 
+class MultiObject(Display):
+    """
+    Generate a MultiObject view which axes will be the given attributes.
+
+    It Will show a Scatter and a Parallel Plot.
+
+    :param selector_name: Name of the selector to be displayed in object page.
+        Must be unique throughout workflow.
+    :param attributes: A List of all attributes that will be shown on axes in the ParallelPlot window.
+        Can be deep attributes with the '/' separator.
+    :param name: Name of the block.
+    :param position: Position of the block in canvas.
+    """
+
+    _type = "plot_data"
+    serialize = True
+
+    def __init__(self, selector_name: str, attributes: List[AttributeType[Type]], load_by_default: bool = True,
+                 name: str = "Multi Object View", position:  Position = (0, 0)):
+        self.attributes = attributes
+        Display.__init__(self, inputs=[Variable(type_=List[DessiaObject])], load_by_default=load_by_default,
+                         name=name, selector=PlotDataType(class_=DessiaObject, name=selector_name), position=position)
+        self.inputs[0].name = "Sequence"
+
+    def __deepcopy__(self, memo=None):
+        return MultiObject(selector_name=self.selector.name, attributes=self.attributes,
+                           load_by_default=self.load_by_default, name=self.name, position=self.position)
+
+    def equivalent(self, other):
+        """ Return whether if the block is equivalent to the other given. """
+        same_attributes = self.attributes == other.attributes
+        return super().equivalent(other) and same_attributes
+
+    def equivalent_hash(self):
+        """ Custom hash function. Related to 'equivalent' method. """
+        return sum(hash(a) for a in self.attributes)
+
+    def evaluate(self, values, **kwargs):
+        """ Create MultiPlot from block configuration. Handle reference path. """
+        reference_path = kwargs.get("reference_path", "#")
+        import plot_data
+        objects = values[self.inputs[self._displayable_input]]
+        samples = [plot_data.Sample(values={a: get_in_object_from_path(o, a) for a in self.attributes},
+                                    reference_path=f"{reference_path}/{i}", name=f"Sample {i}")
+                   for i, o in enumerate(objects)]
+        samples2d = [plot_data.Sample(values={a: get_in_object_from_path(o, a) for a in self.attributes[:2]},
+                                      reference_path=f"{reference_path}/{i}", name=f"Sample {i}")
+                     for i, o in enumerate(objects)]
+        attribute_names = [a.name for a in self.attributes]
+        tooltip = plot_data.Tooltip(name="Tooltip", attributes=attribute_names)
+
+        scatterplot = plot_data.Scatter(tooltip=tooltip, x_variable=attribute_names[0], y_variable=attribute_names[1],
+                                        elements=samples2d, name="Scatter Plot")
+
+        parallelplot = plot_data.ParallelPlot(disposition="horizontal", axes=attribute_names,
+                                              rgbs=[(192, 11, 11), (14, 192, 11), (11, 11, 192)], elements=samples)
+        plots = [scatterplot, parallelplot]
+        sizes = [plot_data.Window(width=560, height=300), plot_data.Window(width=560, height=300)]
+        multiplot = plot_data.MultiplePlots(elements=samples, plots=plots, sizes=sizes,
+                                            coords=[(0, 0), (0, 300)], name="Results plot")
+        return [multiplot.to_dict()]
+
+    def _to_script(self, _) -> ToScriptElement:
+        """ Write block config into a chunk of script. """
+        script = (f"MultiObject("
+                  f"selector_name='{self.selector.name}',"
+                  f" attributes={self.attributes},"
+                  f" {self.base_script()})")
+        return ToScriptElement(declaration=script, imports=[self.full_classname])
+
+    def to_dict(self, use_pointers: bool = True, memo=None, path: str = '#',
+                id_method=True, id_memo=None, **kwargs) -> JsonSerializable:
+        """ Overwrite to_dict method in order to handle difference of behaviors about selector. """
+        dict_ = super().to_dict(use_pointers=use_pointers, memo=memo, path=path, id_method=id_method, id_memo=id_memo)
+        dict_.update({"selector_name": self.selector.name, "attributes": [a.to_dict() for a in self.attributes],
+                      "name": self.name, "load_by_default": self.load_by_default, "position": self.position})
+        return dict_
+
+    @classmethod
+    def dict_to_object(cls, dict_: JsonSerializable, **kwargs) -> 'MultiObject':
+        """ Backward compatibility for old versions of Display blocks. """
+        attributes = [AttributeType.dict_to_object(a) for a in dict_["attributes"]]
+        block = MultiObject(selector_name=dict_["selector_name"], attributes=attributes, name=dict_["name"],
+                            load_by_default=dict_["load_by_default"], position=dict_["position"])
+        block.dict_to_inputs(dict_)
+        return block
+
+
 class DeprecatedCadView(Display):
     """
     Deprecated version of CadView block.
