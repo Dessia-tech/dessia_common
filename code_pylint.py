be808484ac22630a1158e28f12539f0aafc2f4e1
GhislainJ <ghislain.jezequel@gmail.com>
2024-06-14 15:27:44 +0200

    chore(pylint): raise threshold

--- code_pylint.py
+++ code_pylint.py
@@ -18,7 +18,7 @@ from datetime import date
 from pylint import __version__
 from pylint.lint import Run
 
-MIN_NOTE = 9.5
+MIN_NOTE = 9.7
 
 EFFECTIVE_DATE = date(2023, 1, 18)
 WEEKLY_DECREASE = 0.03
@@ -31,7 +31,6 @@ MAX_ERROR_BY_TYPE = {
     "too-many-locals": 5,  # Reduce by dropping vectored objects
     "too-many-branches": 8,  # Huge refactor needed. Will be reduced by schema refactor
     "unused-argument": 3,  # Some abstract functions have unused arguments (plot_data). Hence cannot decrease
-    "cyclic-import": 0,  # Still work to do on Specific based DessiaObject
     "too-many-arguments": 18,  # Huge refactor needed
     "too-few-public-methods": 4,  # Abstract classes (Errors, Checks,...)
     "too-many-return-statements": 7,  # Huge refactor needed. Will be reduced by schema refactor
