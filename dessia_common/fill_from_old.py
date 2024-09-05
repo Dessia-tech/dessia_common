e99cdb852158711111e9e4cc717f29055a04321b
Lucas Launay <launay@dessia.tech>
2024-09-05 19:37:03 +0200

    fix: list assembly

--- dessia_common/fill_from_old.py
+++ dessia_common/fill_from_old.py
@@ -4,13 +4,13 @@ from typing import List
 
 class ObjectTest(DessiaObject):
     _standalone_in_db = True
-    def __init__(self, value_1: float, name= 'ObjectTest'):
+    def __init__(self, value_1: float, name: str= 'ObjectTest'):
         self.value_1 = value_1
         DessiaObject.__init__(self, name=name)
 
 class AssemblyObjectTest(DessiaObject):
     _standalone_in_db = True
-    def __init__(self, object_test: ObjectTest, value_2: float, name= 'ObjectTest'):
+    def __init__(self, object_test: ObjectTest, value_2: float, name: str= 'ObjectTest'):
         self.object_test = object_test
         self.value_2 = value_2
         DessiaObject.__init__(self, name=name)
@@ -18,6 +18,6 @@ class AssemblyObjectTest(DessiaObject):
 
 class ListAssemblyObjectTest(DessiaObject):
     _standalone_in_db = True
-    def __init__(self, list_assembly: List[AssemblyObjectTest], name='ObjectTest'):
+    def __init__(self, list_assembly: List[AssemblyObjectTest], name: str='ObjectTest'):
         self.list_assembly = list_assembly
         DessiaObject.__init__(self, name=name)
