df79d6ba1e18f187ca08044e224ae869ad997764
Lucas Launay <launay@dessia.tech>
2024-09-05 19:33:44 +0200

    fix: standaolne indb

--- dessia_common/fill_from_old.py
+++ dessia_common/fill_from_old.py
@@ -3,13 +3,13 @@ from typing import List
 
 
 class ObjectTest(DessiaObject):
-
+    _standalone_in_db = True
     def __init__(self, value_1: float, name= 'ObjectTest'):
         self.value_1 = value_1
         DessiaObject.__init__(self, name=name)
 
 class AssemblyObjectTest(DessiaObject):
-
+    _standalone_in_db = True
     def __init__(self, object_test: ObjectTest, value_2: float, name= 'ObjectTest'):
         self.object_test = object_test
         self.value_2 = value_2
@@ -17,7 +17,7 @@ class AssemblyObjectTest(DessiaObject):
 
 
 class ListAssemblyObjectTest(DessiaObject):
-
+    _standalone_in_db = True
     def __init__(self, list_assembly: List[AssemblyObjectTest], name='ObjectTest'):
         self.list_assembly = list_assembly
         DessiaObject.__init__(self, name=name)
