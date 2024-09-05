5caaaa9647f40fcb2315c8cab0f4cea23d063b35
Lucas Launay <launay@dessia.tech>
2024-09-05 19:21:50 +0200

    test: fill fom old

---
+++ dessia_common/fill_from_old.py
@@ -0,0 +1,22 @@
+from dessia_common.core import DessiaObject
+from typing import List
+class ObjectTest(DessiaObject):
+
+    def __init__(self, value_1: float, name= 'ObjectTest'):
+        self.value_1 = value_1
+        DessiaObject.__init__(self, name=name)
+
+class AssemblyObjectTest(DessiaObject):
+
+    def __init__(self, object_test: ObjectTest, value_2: float, name= 'ObjectTest'):
+        self.object_test = object_test
+        self.value_2 = value_2
+        DessiaObject.__init__(self, name=name)
+
+
+class ListAssemblyObjectTest(DessiaObject):
+
+    def __init__(self, list_assembly: List[AssemblyObjectTest], name='ObjectTest'):
+        self.list_assembly = list_assembly
+        DessiaObject.__init__(self, name=name)
+        
